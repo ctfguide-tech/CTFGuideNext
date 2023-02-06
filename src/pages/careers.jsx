be1a3160fe55423792420f3dcf30555b846ccdca
Pranav Ramesh <laphatize@protonmail.com>
2023-02-06 14:19:00 -0500

    Career page changes

--- src/pages/careers.jsx
+++ src/pages/careers.jsx
@@ -8,10 +8,35 @@ import { Stats } from '@/components/dashboard/Stats'
 import { Developer } from '@/components/dashboard/Developer'
 import { useEffect } from 'react'
 
+import { Image } from 'next/image'
+
 export default function Careers(){
 /*
       Code to check if onboarding has been complete
     */
+   
+        {/* Each person will be an object in an array team */}
+        const team = [
+            
+            {
+                personName: "Pranav Ramesh" ,
+                position: "CEO", 
+                image: "../pranavCTF.jpeg",
+                width: "200"
+            },
+                
+            {
+                personName: "Abhinav Byreddy",
+                position: "Systems Architect",
+                image: "../abhi.jpeg",
+                width: "200"
+            }
+
+            
+      ]
+
+
+
       useEffect(() => {
         fetch("api.ctfguide.com/dashboard")
           .then((res) => res.json())
@@ -25,6 +50,7 @@ export default function Careers(){
       //  .catch((error) => window.location.replace("http://localhost:3000/onboarding?part=1"))
       })
   
+ 
     return (
         <>
         <Head>
@@ -37,30 +63,36 @@ export default function Careers(){
           @import url(&apos;https://fonts.googleapis.com/css2?family=Poppins&display=swap&apos;);
         </style>
         </Head>
-        const pranav = require("src\images\people\pranavCTF.jpeg")
-        const abhi = require("src\images\people\abhi.jpeg")
-
-        {/* Each person will be an object in an array team */}
-        const team = [
-            
-            {
-                personName: "Pranav Ramesh" ,
-                position: "CEO", 
-                image: pranav,
-            },
-                
-            {
-                personName: "Abhinav Byreddy",
-                position: "Systems Architect",
-                image: abhi,
-            },
-
-        ]
+       
 
 
         <StandardNav/>
         <main>
+  
 
+          <h1 className='text-3xl text-white'> Our Team </h1>
+
+         
+         {
+           /*
+          sm is for "smaller" devices
+          */
+         }
+          <div className='grid grid-cols-3 gap-4 mx-auto text-center'>
+            { 
+                team.map((person) => {
+                    return (
+                        <div className='text-white '>
+                            <img className={' rounded-full mx-auto'} width={person.width} src={person.image}></img>
+                            <h1>{person.personName}</h1>
+                            <h2>{person.position}</h2>
+                        </div>
+                    )
+                })
+            }
+
+</div>  
+            
         
         </main>
 
