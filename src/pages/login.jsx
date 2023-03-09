87ef52ea518461c3eb436659adc63288d6a23baf
Pranav Ramesh <laphatize@protonmail.com>
2023-03-09 02:30:14 -0500

    Registration and Login fixed

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,8 +6,9 @@ import { Logo } from '@/components/Logo'
 import { Alert } from '@/components/Alert'
 import { useState, useEffect } from "react"
 import { app } from '../config/firebaseConfig';
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth"
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 
+const provider = new GoogleAuthProvider();
 
 export default function Login() {
   const auth = getAuth();
@@ -18,7 +19,6 @@ export default function Login() {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         const uid = user.uid;
-        //  window.location.replace("/dashboard")
       }
     });
   }, [])
@@ -35,7 +35,6 @@ export default function Login() {
         // Fetch ID Token
         userCredential.user.getIdToken().then((idToken) => {
           // Send token to backend via HTTPS
-          console.log(idToken)
           var data = new FormData();
           var xhr = new XMLHttpRequest();
 
@@ -78,6 +77,66 @@ export default function Login() {
 
   }
 
+  async function loginGoogle() {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+       
+
+           // Fetch ID Token
+           result.user.getIdToken().then((idToken) => {
+            // Send token to backend via HTTPS
+            console.log(idToken)
+            var data = new FormData();
+            var xhr = new XMLHttpRequest();
+  
+
+            xhr.open("GET", `${process.env.NEXT_PUBLIC_API_URL}/account`);
+            xhr.addEventListener("readystatechange", function () {
+              if (this.readyState === 4) {
+                var parsed = JSON.parse(this.responseText);
+                
+                // Store Token in local storage.
+                localStorage.setItem("idToken", idToken);
+
+                if (!parsed.email) {
+                  // User hasn't finished onboarding.
+                  window.location.replace("/onboarding");
+                  return;
+                }
+
+
+  
+                // Store related API endpoints in local storage.
+                localStorage.setItem("userLikesUrl", parsed.userLikesUrl);
+                localStorage.setItem("userChallengesUrl", parsed.userChallengesUrl);
+                localStorage.setItem("userBadgesUrl", parsed.userBadgesUrl);
+                localStorage.setItem("notificationsUrl", parsed.notificationsUrl);
+                localStorage.setItem("email", parsed.email);
+                localStorage.setItem("role", parsed.role);
+  
+  
+              
+  
+              }
+            });
+            xhr.setRequestHeader("Authorization", "Bearer " + idToken);
+            xhr.send(data);
+          }
+          );
+
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        
+        document.getElementById("error").classList.remove("hidden");
+        document.getElementById("errorMessage").innerHTML = errorMessage;
+      });
+  }
+
 
   return (
     <>
@@ -111,7 +170,7 @@ export default function Login() {
           <div style={{ backgroundColor: "#212121" }} className=" py-8 px-4 shadow sm:rounded-lg sm:px-10">
 
             <div className="space-y-6">
-              <div id="error" className="hidden bg-red-500 px-2 py-1 rounded tex†-white">
+              <div id="error" className="hidden bg-red-500 px-4 py-1 rounded tex†-white">
                 <h1 className='text-white text-center' id="errorMessage">Something went wrong.</h1>
               </div>
               <div>
@@ -170,52 +229,22 @@ export default function Login() {
             <div className="mt-6">
 
 
-              <div className="mt-6 grid grid-cols-3 gap-3 ">
+              <div className="mt-6  gap-3 ">
                 <div>
                   <a
                     style={{ backgroundColor: "#161716", borderWidth: "0px" }}
                     href="#"
                     className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                    onClick={loginGoogle}
                   >
-                    <span className="sr-only">Sign in with Facebook</span>
-                    <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                      <path
-                        fillRule="evenodd"
-                        d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    <span className="sr-only">Sign in with Google</span>
+                    <svg className='h-6 w-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48px" height="48px"><path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" /><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" /><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" /><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z" /></svg>
+                    <p className='ml-2'>Login with Google</p>
                   </a>
                 </div>
 
-                <div>
-                  <a
-                    style={{ backgroundColor: "#161716", borderWidth: "0px" }}
-                    href="#"
-                    className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-                  >
-                    <span className="sr-only">Sign in with Twitter</span>
-                    <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
-                  </a>
-                </div>
 
-                <div>
-                  <a style={{ backgroundColor: "#161716", borderWidth: "0px" }}
-                    href="#"
-                    className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-                  >
-                    <span className="sr-only">Sign in with GitHub</span>
-                    <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                      <path
-                        fillRule="evenodd"
-                        d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </a>
-                </div>
+
               </div>
             </div>
           </div>
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { app } from '../config/firebaseConfig';
+
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+
 
 export default function Register() {
 
@@ -11,8 +15,26 @@ export default function Register() {
         .then((userCredential) => {
           // Signed in 
           const user = userCredential.user;
-          localStorage.setItem("token", user.uid);
-          window.location.href = "/onboarding"
+          localStorage.setItem("idToken", user.uid);
+          userCredential.user.getIdToken().then((idToken) => {
+
+
+
+            xhr.open("GET", `${process.env.NEXT_PUBLIC_API_URL}/account`);
+            xhr.addEventListener("readystatechange", function () {
+              if (this.readyState === 4) {
+                var parsed = JSON.parse(this.responseText);
+
+                // Store Token in local storage.
+                localStorage.setItem("idToken", idToken);
+
+                window.location.replace("/onboarding");
+
+              }
+            });
+
+          }
+          );
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -34,158 +56,158 @@ export default function Register() {
           @import url(&apos;https://fonts.googleapis.com/css2?family=Poppins&display=swap&apos;);
         </style>
       </Head>
-    
+
       <div style={{ fontFamily: "Poppins, sans-serif", height: "100vh" }} className="flex min-h-full animate__animated animate__fadeIn">
-      <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-        <div className=" mx-auto w-full max-w-sm lg:w-96">
-          <div>
-            <Link href="../" className="my-auto flex">
-              <img
-                className="h-10 w-10 zimg"
-                src="../darkLogo.png"
-                alt="CTFGuide"
-              />
-              <h1 className='text-white text-xl my-auto'>CTFGuide</h1>
-            </Link>
-            <h2 className="mt-6 text-3xl font-bold tracking-tight text-white">Register</h2>
-            <p className="mt-2 text-sm text-gray-600">
-              Or{' '}
-              <a href="./login" className="font-medium text-blue-600 hover:text-blue-500">
-                login to your account
-              </a>
-            </p>
-          </div>
-
-          <div className="mt-8">
+        <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className=" mx-auto w-full max-w-sm lg:w-96">
             <div>
-              <div hidden>
-                <p className="text-sm font-medium text-white">Sign up with</p>
-
-                <div className="mt-2 grid grid-cols-1 gap-3 hidden">
-
-
-                  <div>
-                    <a
-                      style={{ backgroundColor: "#212121" }}
-                      href="#"
-                      className="inline-flex w-full justify-center rounded-md   py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-                    >
-                      <span className="sr-only">Sign up with Google</span>
-                      <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                        <clipPath id="p.0"> <path d="m0 0l20.0 0l0 20.0l-20.0 0l0 -20.0z" clipRule="nonzero" /> </clipPath> <g clipPath="url(#p.0)"> <path fill="currentColor" fillOpacity="0.0" d="m0 0l20.0 0l0 20.0l-20.0 0z" fillRule="evenodd" /> <path fill="currentColor" d="m19.850197 8.270351c0.8574047 4.880001 -1.987587 9.65214 -6.6881847 11.218641c-4.700598 1.5665016 -9.83958 -0.5449295 -12.08104 -4.963685c-2.2414603 -4.4187555 -0.909603 -9.81259 3.1310139 -12.6801605c4.040616 -2.867571 9.571754 -2.3443127 13.002944 1.2301085l-2.8127813 2.7000687l0 0c-2.0935059 -2.1808972 -5.468274 -2.500158 -7.933616 -0.75053835c-2.4653416 1.74962 -3.277961 5.040613 -1.9103565 7.7366734c1.3676047 2.6960592 4.5031037 3.9843292 7.3711267 3.0285425c2.868022 -0.95578575 4.6038647 -3.8674583 4.0807285 -6.844941z" fillRule="evenodd" /> <path fill="currentColor" d="m10.000263 8.268785l9.847767 0l0 3.496233l-9.847767 0z" fillRule="evenodd" /> </g>
-                      </svg>
-                    </a>
-                  </div>      
-                </div>
-              </div>
-
-              <div className="hidden relative mt-6">
-                <div className="relative flex justify-center text-sm">
-                  <span className=" px-2 text-gray-500">Or use your email</span>
-                </div>
-              </div>
+              <Link href="../" className="my-auto flex">
+                <img
+                  className="h-10 w-10 zimg"
+                  src="../darkLogo.png"
+                  alt="CTFGuide"
+                />
+                <h1 className='text-white text-xl my-auto'>CTFGuide</h1>
+              </Link>
+              <h2 className="mt-6 text-3xl font-bold tracking-tight text-white">Register</h2>
+              <p className="mt-2 text-sm text-gray-600">
+                Or{' '}
+                <a href="./login" className="font-medium text-blue-600 hover:text-blue-500">
+                  login to your account
+                </a>
+              </p>
             </div>
 
-            <div className="mt-4">
-              <div className="space-y-6">
-                <div id="error" className="hidden text-white bg-blue-900 animate_animated animate__fadeIn border border-blue-500 px-2 py-2 rounded-lg">
-                  <p className="text-white text-sm text-center">There was an error when trying to log you in.</p>
-                </div>
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium text-white">
-                    Email address
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      style={{ backgroundColor: "#212121", borderStyle: "none" }}
-                      id="email-address"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="block w-full appearance-none rounded-md text-white  px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                    />
+            <div className="mt-8">
+              <div>
+                <div hidden>
+                  <p className="text-sm font-medium text-white">Sign up with</p>
+
+                  <div className="mt-2 grid grid-cols-1 gap-3 hidden">
+
+
+                    <div>
+                      <a
+                        style={{ backgroundColor: "#212121" }}
+                        href="#"
+                        className="inline-flex w-full justify-center rounded-md   py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                      >
+                        <span className="sr-only">Sign up with Google</span>
+                        <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                          <clipPath id="p.0"> <path d="m0 0l20.0 0l0 20.0l-20.0 0l0 -20.0z" clipRule="nonzero" /> </clipPath> <g clipPath="url(#p.0)"> <path fill="currentColor" fillOpacity="0.0" d="m0 0l20.0 0l0 20.0l-20.0 0z" fillRule="evenodd" /> <path fill="currentColor" d="m19.850197 8.270351c0.8574047 4.880001 -1.987587 9.65214 -6.6881847 11.218641c-4.700598 1.5665016 -9.83958 -0.5449295 -12.08104 -4.963685c-2.2414603 -4.4187555 -0.909603 -9.81259 3.1310139 -12.6801605c4.040616 -2.867571 9.571754 -2.3443127 13.002944 1.2301085l-2.8127813 2.7000687l0 0c-2.0935059 -2.1808972 -5.468274 -2.500158 -7.933616 -0.75053835c-2.4653416 1.74962 -3.277961 5.040613 -1.9103565 7.7366734c1.3676047 2.6960592 4.5031037 3.9843292 7.3711267 3.0285425c2.868022 -0.95578575 4.6038647 -3.8674583 4.0807285 -6.844941z" fillRule="evenodd" /> <path fill="currentColor" d="m10.000263 8.268785l9.847767 0l0 3.496233l-9.847767 0z" fillRule="evenodd" /> </g>
+                        </svg>
+                      </a>
+                    </div>
                   </div>
                 </div>
 
-                <div className="space-y-1">
-                  <label htmlFor="password" className="block text-sm font-medium text-white">
-                    Password
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      style={{ backgroundColor: "#212121", borderStyle: "none" }}
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      className="block w-full appearance-none text-white rounded-md  px-3 py-2  shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                    />
+                <div className="hidden relative mt-6">
+                  <div className="relative flex justify-center text-sm">
+                    <span className=" px-2 text-gray-500">Or use your email</span>
                   </div>
                 </div>
+              </div>
 
-                <div className="space-y-1">
-                  <label htmlFor="password" className="block text-sm font-medium text-white">
-                   Confirm Password
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      style={{ backgroundColor: "#212121", borderStyle: "none" }}
-                      id="cpassword"
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      className="block w-full appearance-none text-white rounded-md  px-3 py-2  shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                    />
+              <div className="mt-4">
+                <div className="space-y-6">
+                  <div id="error" className="hidden text-white bg-blue-900 animate_animated animate__fadeIn border border-blue-500 px-2 py-2 rounded-lg">
+                    <p className="text-white text-sm text-center">There was an error when trying to log you in.</p>
                   </div>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center">
-                    <input
-                      style={{ backgroundColor: "#212121", borderStyle: "none" }}
-                      id="remember-me"
-                      name="remember-me"
-                      type="checkbox"
-                      className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-                    />
-                    <label htmlFor="remember-me" className="ml-2 block text-sm text-white">
-                      Remember me
+                  <div>
+                    <label htmlFor="email" className="block text-sm font-medium text-white">
+                      Email address
                     </label>
+                    <div className="mt-1">
+                      <input
+                        style={{ backgroundColor: "#212121", borderStyle: "none" }}
+                        id="email-address"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full appearance-none rounded-md text-white  px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
                   </div>
 
-                  <div className="text-sm">
-                    <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
-                      Forgot your password?
-                    </a>
+                  <div className="space-y-1">
+                    <label htmlFor="password" className="block text-sm font-medium text-white">
+                      Password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        style={{ backgroundColor: "#212121", borderStyle: "none" }}
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full appearance-none text-white rounded-md  px-3 py-2  shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div>
-                  <button
-                    type="submit"
-                    onClick={registerUser}
-                    id="registerBtn"
-                    className="flex w-full justify-center rounded-md border border-transparent bg-blue-700 hover:shadow-lg hover:shadow-blue-500/00 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none "
-                  >
-                    Create an account
-                  </button>
+                  <div className="space-y-1">
+                    <label htmlFor="password" className="block text-sm font-medium text-white">
+                      Confirm Password
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        style={{ backgroundColor: "#212121", borderStyle: "none" }}
+                        id="cpassword"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full appearance-none text-white rounded-md  px-3 py-2  shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <input
+                        style={{ backgroundColor: "#212121", borderStyle: "none" }}
+                        id="remember-me"
+                        name="remember-me"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                      />
+                      <label htmlFor="remember-me" className="ml-2 block text-sm text-white">
+                        Remember me
+                      </label>
+                    </div>
+
+                    <div className="text-sm">
+                      <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
+                        Forgot your password?
+                      </a>
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      onClick={registerUser}
+                      id="registerBtn"
+                      className="flex w-full justify-center rounded-md border border-transparent bg-blue-700 hover:shadow-lg hover:shadow-blue-500/00 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none "
+                    >
+                      Create an account
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="relative  w-0 flex-1 lg:block">
+          <img
+            className="absolute inset-0 h-full w-full object-cover "
+            src="https://images.unsplash.com/photo-1633259584604-afdc243122ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt=""
+          />
+        </div>
       </div>
-      <div className="relative  w-0 flex-1 lg:block">
-        <img
-          className="absolute inset-0 h-full w-full object-cover "
-          src="https://images.unsplash.com/photo-1633259584604-afdc243122ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          alt=""
-        />
-      </div>
-    </div>
     </>
   )
 }
