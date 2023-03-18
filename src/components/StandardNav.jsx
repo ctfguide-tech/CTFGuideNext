f1b0b4e95cd4cc386142992b809e3c4b2c0bf257
Pranav Ramesh <laphatize@protonmail.com>
2023-03-18 16:12:12 -0400

    Update StandardNav.jsx

--- src/components/StandardNav.jsx
+++ src/components/StandardNav.jsx
@@ -265,7 +265,7 @@ export function StandardNav() {
                         <Menu.Item>
                           {({ active }) => (
                             <a
-                              href="https://feedback.ctfguide.com/"
+                              href="https://ctfguide.hellonext.co/b/feedback"
                               className={classNames(
                                 active ? 'bg-gray-100' : '',
                                 'block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 flex'
