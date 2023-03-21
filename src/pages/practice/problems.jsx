516f7c32f574b9bae9ddcc666466aefd8431580a
Pranav Ramesh <laphatize@protonmail.com>
2023-03-21 15:30:38 -0400

    Functional Problem sets

--- src/pages/practice/problems.jsx
+++ src/pages/practice/problems.jsx
@@ -5,6 +5,10 @@ import { Footer } from '@/components/Footer'
 import { PracticeNav } from '@/components/practice/PracticeNav'
 import { ProblemSetCards } from '@/components/practice/GoToCreate'
 import ProblemSet from '@/components/practice/ProblemSet'
+import { ChallengeCard } from '@/components/create/ChallengeCard';
+import { Community } from '@/components/practice/community'
+
+import Challenge from "@/components/challenge/ChallengeComponent";
 
 export default function ProblemsPage() {
     const [components, setComponents] = useState([]);
@@ -23,6 +27,9 @@ export default function ProblemsPage() {
         }
     ];
 
+    const [cryptoChallenges, setCryptoChallenges] = useState([]);
+
+
     useEffect(() => {
         try {
             fetch(process.env.NEXT_PUBLIC_API_URL + "/challenges")
@@ -36,9 +43,33 @@ export default function ProblemsPage() {
                 .catch((error) => {
                     console.error(error);
                 });
+
+           
+      
         } catch {
 
         }
+
+  
+
+
+    }, []);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // const response = await fetch('https://api.ctfguide.com/challenges/type/all');
+                // const data = await response.json();
+                // setChallenges([...data]);
+                const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/challenges?category=cryptography');
+                const { result } = await response.json();
+                
+                setCryptoChallenges([...result]);
+            } catch (err) {
+                console.log (err);
+            }
+        };
+        fetchData();        
     }, []);
 
     const filterData = (category) => {
@@ -48,6 +79,19 @@ export default function ProblemsPage() {
             })
         });
     }
+
+    const badgeColor = {
+        'easy': 'bg-[#28a745] text-[#212529] ',
+        'medium': 'bg-[#f0ad4e] text-[#212529] ',
+        'hard': 'bg-[#dc3545] ',
+    }
+
+    const borderColor = {
+        'easy': 'border-[#28a745] text-[#212529] ',
+        'medium': 'border-[#f0ad4e] text-[#212529] ',
+        'hard': 'border-[#dc3545] ',
+    }
+
 
     return (
         <>
@@ -85,6 +129,38 @@ export default function ProblemsPage() {
                                 </div>
                             </div>
                         ))}
+
+                        <div>
+                        <h2 className="mt-8 text-center text-3xl font-bold tracking-tight text-white">Cryptography</h2>
+                                <p className="text-gray-300 text-center mt-2">Cryptography deals with algorithms to secure info. Encryption/decryption are the foundation of cybersecurity.</p>
+                             
+
+                                <div className="w-full overflow-x-scroll pb-4">
+  <div className="flex gap-x-5 mt-3" style={{ width: "fit-content" }}>                             {
+                            cryptoChallenges.map((data) => (
+                                <a 
+                                href={{
+                                    pathname: "/challenge",
+                                    query: {
+                                        slug: data.slug
+                                    }
+                                }}
+                                className=""
+                                >
+                                <div className={"min-h-[190px] min-w-[200px] w-full ml-4 flex-shrink-0 cursor-pointer text-white bg-neutral-800 hover:bg-neutral-800 font-semibold rounded-lg px-3 py-2  backdrop-blur-lg py-4 border-t-8 " + borderColor[data.difficulty.toLowerCase()]}
+>
+                                    { /* difficulty in red, green or yellow */ }
+                                    <span className={'text-white px-2 rounded-lg font-semibold bg-blue-900 text-sm mr-2 mt-1 ' + badgeColor[data.difficulty.toLowerCase()]}>{data.difficulty}</span>
+
+                                    <h3 className='text-white  font-bold truncate mt-2 text-2xl'>{data.title.substring(0, 45)}</h3>
+                                    <p className='text-white truncate text-md mt-1'>{data.content.substring(0, 40)}</p>
+                                  
+                                </div>
+                            </a>                            ))
+                        }
+                        </div>
+                             </div>
+                        </div>
                     </div>
                 </div>
             </main>
