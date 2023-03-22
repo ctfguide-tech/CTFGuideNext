738f3fec36b8e5eadfbad8c444712c8642f2ee86
Pranav Ramesh <laphatize@protonmail.com>
2023-03-22 02:33:28 -0400

    prepare for prod

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -11,7 +11,7 @@ import { initializeApp } from "firebase/app";
         It isn't some secret key or anything. It's just a way for our auth service (Firebase) to identify the app.
 */
 
-
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyAHz1s-UuNhlZ6aKvqwzmzzidzWxBKw9hw",
   authDomain: "ctfguide-dev.firebaseapp.com",
@@ -19,15 +19,14 @@ const firebaseConfig = {
   storageBucket: "ctfguide-dev.appspot.com",
   messagingSenderId: "792987058367",
   appId: "1:792987058367:web:c48935325e46043c3cc60a"
-};
+};*/
 
-const app = initializeApp(firebaseConfig);  
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 
 
-/*
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBLAN84VP3jSA5dqhrU6Bjmfu5NiUDuNw4",
@@ -39,7 +38,10 @@ const firebaseConfig = {
   appId: "1:166652277588:web:e08b9e19916451e14dcec1",
   measurementId: "G-7ZNKM9VFN2"
 };
-*/
+
+
+const app = initializeApp(firebaseConfig);  
+
 
 
 // Initialize Firebase
