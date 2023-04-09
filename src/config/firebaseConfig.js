ae8dd369801cf6083ab8eb2b5e79d0eaa963d4a1
Jaiming724 <Jiamingw29@gmail.com>
2023-04-09 14:16:17 -0400

    removed insights

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -11,7 +11,7 @@ import { initializeApp } from "firebase/app";
         It isn't some secret key or anything. It's just a way for our auth service (Firebase) to identify the app.
 */
 
-/*
+//
 const firebaseConfig = {
   apiKey: "AIzaSyAHz1s-UuNhlZ6aKvqwzmzzidzWxBKw9hw",
   authDomain: "ctfguide-dev.firebaseapp.com",
@@ -19,7 +19,7 @@ const firebaseConfig = {
   storageBucket: "ctfguide-dev.appspot.com",
   messagingSenderId: "792987058367",
   appId: "1:792987058367:web:c48935325e46043c3cc60a"
-};*/
+};
 
 
 // Initialize Firebase
@@ -28,16 +28,16 @@ const firebaseConfig = {
 
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBLAN84VP3jSA5dqhrU6Bjmfu5NiUDuNw4",
-  authDomain: "cyberjags-8b081.firebaseapp.com",
-  databaseURL: "https://cyberjags-8b081.firebaseio.com",
-  projectId: "cyberjags-8b081",
-  storageBucket: "cyberjags-8b081.appspot.com",
-  messagingSenderId: "166652277588",
-  appId: "1:166652277588:web:e08b9e19916451e14dcec1",
-  measurementId: "G-7ZNKM9VFN2"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBLAN84VP3jSA5dqhrU6Bjmfu5NiUDuNw4",
+//   authDomain: "cyberjags-8b081.firebaseapp.com",
+//   databaseURL: "https://cyberjags-8b081.firebaseio.com",
+//   projectId: "cyberjags-8b081",
+//   storageBucket: "cyberjags-8b081.appspot.com",
+//   messagingSenderId: "166652277588",
+//   appId: "1:166652277588:web:e08b9e19916451e14dcec1",
+//   measurementId: "G-7ZNKM9VFN2"
+// };
 
 
 const app = initializeApp(firebaseConfig);  
