61ffc06aef8d60e683c18d804e5b1269c7abbdf8
Pranav Ramesh <laphatize@protonmail.com>
2023-04-11 20:27:11 -0400

    Fix sign up with google btn

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -163,7 +163,7 @@ export default function Register() {
                     <div>
                       <a
                         href="#"
-                        className="inline-flex w-full justify-center rounded-md bg-neutral-800  bg-white py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-neutral-700"
+                        className="inline-flex w-full justify-center rounded-md bg-neutral-800   py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-neutral-700"
                         onClick={loginGoogle}
                       >
                         <span className="sr-only">Sign in with Google</span>
