330ffeac06be9af9d484bb897b5cb8dad54a7dbd
Pranav Ramesh <laphatize@protonmail.com>
2023-04-21 23:40:32 -0400

    Remove duplicate and improved mobile navigation

--- src/components/StandardNav.jsx
+++ src/components/StandardNav.jsx
@@ -25,7 +25,7 @@ function classNames(...classes) {
 
 const auth = getAuth();
 
-
+const adminList = ["pranav,"]
 const DEFAULT_NOTIFICATION = {
   image:
     'https://cutshort-data.s3.amazonaws.com/cloudfront/public/companies/5809d1d8af3059ed5b346ed1/logo-1615367026425-logo-v6.png',
@@ -150,7 +150,7 @@ export function StandardNav() {
               <div className="flex">
                 <div className="-ml-2 mr-2 flex items-center md:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover-bg-neutral-900 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -194,12 +194,7 @@ export function StandardNav() {
                   >
                     Create
                   </a>
-                  <a
-                    href="../../create"
-                    className="inline-flex items-center border-b-2 border-transparent px-4 pt-1 text-sm font-medium text-gray-300 hover:font-bold hover:text-gray-200"
-                  >
-                    Create
-                  </a>
+               
                   <a
                     href="../../live"
                     className="inline-flex items-center border-b-2 border-transparent px-4 pt-1 text-sm font-medium text-gray-300 hover:font-bold hover:text-gray-200"
@@ -344,72 +339,58 @@ export function StandardNav() {
           </div>
 
           <Disclosure.Panel className="md:hidden">
-            <div className="space-y-1 pt-2 pb-3">
+            <div className="space-y-1 pt-2 pb-3 bg-neutral-800/50">
               {/* Current: "bg-blue-50 border-blue-500 text-blue-700", Default: "border-transparent text-gray-300 hover:bg-gray-50 hover:font-bold" */}
               <Disclosure.Button
                 as="a"
-                href="#"
-                className="block border-l-4 border-blue-500 bg-blue-50 py-2 pl-3 pr-4 text-base font-medium text-blue-700 sm:pl-5 sm:pr-6"
+                href="../dashboard"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-300 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
               >
                 Dashboard
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
+                href="../learn"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-300 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              >
+                Learn
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="../groups"
                 className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-300 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
               >
                 Groups
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
+                href="../practice"
                 className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-300 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
               >
                 Practice
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="live.ctfguide.com"
+                href="../live"
                 className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-300 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
               >
                 Live
               </Disclosure.Button>
             </div>
-            <div className="border-t border-gray-200 pt-4 pb-3">
-              <div className="flex items-center px-4 sm:px-6">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src={
-                      `https://robohash.org/` +
-                      username +
-                      `.png?set=set1&size=150x150`
-                    }
-                    alt=""
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-gray-800">
-                    Anonymous
-                  </div>
-                  <div className="text-sm font-medium text-gray-300">
-                    anon@ctfguide.com
-                  </div>
-                </div>
-           
-              </div>
-              <div className="mt-3 space-y-1">
+            <div className="border-t border-neutral-800 pt-4 pb-3 bg-neutral-800/50">
+            
+              <div className="space-y-1">
                 <Disclosure.Button
                   as="a"
                   href="#"
-                  className="block px-4 py-2 text-base font-medium text-gray-300 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
+                  className="block px-4 py-2 text-base font-medium text-gray-300 hover-bg-neutral-900 hover:text-gray-800 sm:px-6"
                 >
                   Your Profile
                 </Disclosure.Button>
                 <Disclosure.Button
                   as="a"
                   href="#"
-                  className="block px-4 py-2 text-base font-medium text-gray-300 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
+                  className="block px-4 py-2 text-base font-medium text-gray-300 hover-bg-neutral-900 hover:text-gray-800 sm:px-6"
                 >
                   Settings
                 </Disclosure.Button>
@@ -417,7 +398,7 @@ export function StandardNav() {
                   as="a"
                   href="#"
                   onClick={{ logout }}
-                  className="block px-4 py-2 text-base font-medium text-gray-300 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
+                  className="block px-4 py-2 text-base font-medium text-gray-300 hover-bg-neutral-900 hover:text-gray-800 sm:px-6"
                 >
                   Sign out
                 </Disclosure.Button>
