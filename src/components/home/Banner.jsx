efbdf6ed6f876b319689da2388226d7e4cc1a8e7
Kshitij Kochhar <kshitij@ctfguide.com>
2023-06-01 12:32:38 -0400

    Updated banner feature w close option

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Banner = () => {
-    const surveyURL = "https://www.google.com";
+    const surveyURL = "https://form.jotform.com/231495033815051";
+    const [showBanner, setShowBanner] = useState(true);
     
-    return (
-        <div className="bg-gradient-to-r from-blue-100 to-purple-200 py-1 flex items-center justify-center">
-            <p className="m-0 mr-2 font-semibold">
-                Please fill out our survey!{' '}
+    const handleCloseBanner = () => {
+        setShowBanner(false);
+        localStorage.setItem('bannerClosed', 'true');
+    };
 
-                <a href={surveyURL} className="hover:text-purple-700 text-blue-700 m1-2">
-                    Click here to help!
-                </a>
-            </p>
+    useEffect(() => {
+        const isBannerClosed = localStorage.getItem('bannerClosed');
+        if (isBannerClosed === 'true') {
+            setShowBanner(false);
+        }
+    }, []);
+
+      return (
+        <div>
+        {showBanner && (
+            <div className="bg-neutral-800 py-1 flex items-center justify-between">
+                <p className="m-0 text-white text-center flex-grow">
+                    Please fill out our survey!{' '}
+                    <a href={surveyURL} className="hover:text-purple-700 text-blue-600 m1-2 font-semibold">
+                        Click here to help!
+                    </a>
+                </p>
+                <button
+                    id="closeButton"
+                    className="text-white hover:text-gray-700 focus:outline-none
+                        focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 mr-4"
+                    onClick={handleCloseBanner}
+                    aria-describedly="tooltip"
+                >
+                    x
+                </button>
+            </div>
+            )}
         </div>
       );
     };
