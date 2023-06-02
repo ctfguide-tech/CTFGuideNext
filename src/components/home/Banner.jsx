87070b89e3c15a3d0acc6a5dce16fee4308d92c2
Jiaming <41546736+Jaiming724@users.noreply.github.com>
2023-06-01 20:50:44 -0400

    Merge Feat banner (#52)
    * Added banner feature
    
    * Updated banner feature w close option
    
    ---------
    
    Co-authored-by: Raymond Yan <67762433+raymond-design@users.noreply.github.com>
    Co-authored-by: Kshitij Kochhar <kshitij@ctfguide.com>

---
+++ src/components/home/Banner.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+const Banner = () => {
+    const surveyURL = "https://form.jotform.com/231495033815051";
+    const [showBanner, setShowBanner] = useState(true);
+    
+    const handleCloseBanner = () => {
+        setShowBanner(false);
+        localStorage.setItem('bannerClosed', 'true');
+    };
+
+    useEffect(() => {
+        const isBannerClosed = localStorage.getItem('bannerClosed');
+        if (isBannerClosed === 'true') {
+            setShowBanner(false);
+        }
+    }, []);
+
+      return (
+        <div>
+        {showBanner && (
+            <div className="bg-neutral-800 py-1 flex items-center justify-between">
+                <p className="m-0 text-white text-center flex-grow">
+                    Please fill out our survey!{' '}
+                    <a href={surveyURL} className="hover:text-purple-700 text-blue-600 m1-2 font-semibold">
+                        Click here to help!
+                    </a>
+                </p>
+                <button
+                    id="closeButton"
+                    className="text-white hover:text-gray-700 focus:outline-none
+                        focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 mr-4"
+                    onClick={handleCloseBanner}
+                    aria-describedly="tooltip"
+                >
+                    x
+                </button>
+            </div>
+            )}
+        </div>
+      );
+    };
+
+export default Banner;
