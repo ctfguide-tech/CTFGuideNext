e30f8aadc174e1db036fb80acb79ac6f7928a55c
Kshitij Kochhar <kshitij@ctfguide.com>
2023-07-10 20:41:12 -0400

    Updated login screen background

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -147,174 +147,183 @@ export default function Login() {
 
   return (
     <>
-      <Head>
-        <title>Sign In - CTFGuide</title>
-        <style>
-          @import
-          url(&apos;https://fonts.googleapis.com/css2?family=Poppins&display=swap&apos;);
-        </style>
-      </Head>
-
-      <div
-        style={{ fontFamily: 'Poppins, sans-serif' }}
-        className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8"
-      >
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <Link href="../">
-            <img
-              className="mx-auto h-20 w-auto"
-              src="../darkLogo.png"
-              alt="CTFGuide"
-            />
-          </Link>
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
-            Sign in to your account
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            <a
-              href="../register"
-              className="font-semibold text-blue-600 hover:text-blue-500"
+        <Head>
+            <title>Sign In - CTFGuide</title>
+            <style>
+            @import
+            url(&apos;https://fonts.googleapis.com/css2?family=Poppins&display=swap&apos;);
+            </style>
+        </Head>
+        <div style={{
+            backgroundImage: "url('https://images.unsplash.com/photo-1633259584604-afdc243122ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8‌​fHx8&auto=format&fit=crop&w=1770&q=80')",
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            width: '100%',
+            height: '100%',
+        }}>
+            <div
+            style={{ fontFamily: 'Poppins, sans-serif' }}
+            className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8"
             >
-              Don&apos;t have an account?
-            </a>
-          </p>
-        </div>
-
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div
-            style={{ backgroundColor: '#212121' }}
-            className=" py-8 px-4 shadow sm:rounded-lg sm:px-10"
-          >
-            <div className="space-y-6">
-              <div
-                id="error"
-                className="tex†-white hidden rounded bg-red-500 px-4 py-1"
-              >
-                <h1 className="text-center text-white" id="errorMessage">
-                  Something went wrong.
-                </h1>
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-200"
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <Link href="../">
+                <img
+                    className="mx-auto h-20 w-auto"
+                    src="../darkLogo.png"
+                    alt="CTFGuide"
+                />
+                </Link>
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
+                Sign in to your account
+                </h2>
+                <p className="mt-2 text-center text-sm text-gray-600">
+                <a
+                    href="../register"
+                    className="font-semibold text-gray-200 hover:text-gray-400"
                 >
-                  Email
-                </label>
-                <div className="mt-1">
-                  <input
-                    style={{ backgroundColor: '#161716', borderWidth: '0px' }}
-                    id="username"
-                    name="email"
-                    type="text"
-                    autoComplete="email"
-                    required
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-white placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-200"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    style={{ backgroundColor: '#161716', borderWidth: '0px' }}
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-white placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="mx-auto text-center text-sm">
-                  <a
-                    href="./forgot-password"
-                    className="text-center font-medium text-blue-600 hover:text-blue-500"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  onClick={loginUser}
-                  className="flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                >
-                  Sign in
-                </button>
-              </div>
+                    Don&apos;t have an account?
+                </a>
+                </p>
             </div>
 
-            <div className="mt-6">
-              <div className="mt-6  gap-3 ">
-                <div>
-                  <a
-                    style={{ backgroundColor: '#161716', borderWidth: '0px' }}
-                    href="#"
-                    className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-                    onClick={loginGoogle}
-                  >
-                    <span className="sr-only">Sign in with Google</span>
-                    <svg
-                      className="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 48 48"
-                      width="48px"
-                      height="48px"
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div
+                style={{ backgroundColor: '#212121' }}
+                className=" py-8 px-4 shadow sm:rounded-lg sm:px-10"
+                >
+                <div className="space-y-6">
+                    <div
+                    id="error"
+                    className="tex†-white hidden rounded bg-red-500 px-4 py-1"
                     >
-                      <path
-                        fill="#FFC107"
-                        d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-                      />
-                      <path
-                        fill="#FF3D00"
-                        d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-                      />
-                      <path
-                        fill="#4CAF50"
-                        d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-                      />
-                      <path
-                        fill="#1976D2"
-                        d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-                      />
-                    </svg>
-                    <p className="ml-2">Login with Google</p>
-                  </a>
-                </div>
-              </div>
-            </div>
+                    <h1 className="text-center text-white" id="errorMessage">
+                        Something went wrong.
+                    </h1>
+                    </div>
+                    <div>
+                    <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-200"
+                    >
+                        Email
+                    </label>
+                    <div className="mt-1">
+                        <input
+                        style={{ backgroundColor: '#161716', borderWidth: '0px' }}
+                        id="username"
+                        name="email"
+                        type="text"
+                        autoComplete="email"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-white placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    </div>
 
-            <div className="mt-6">
-              <div className="mt-6  gap-3 ">
-                <div>
-                  <a
-                    style={{ backgroundColor: '#161716', borderWidth: '0px' }}
-                    href="#"
-                    className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-                    onClick={loginGoogle}
-                  >
-                    <span className="sr-only">Sign in with Google</span>
-                 
-                    <p className="ml-2">University Login</p>
-                  </a>
+                    <div>
+                    <label
+                        htmlFor="password"
+                        className="block text-sm font-medium text-gray-200"
+                    >
+                        Password
+                    </label>
+                    <div className="mt-1">
+                        <input
+                        style={{ backgroundColor: '#161716', borderWidth: '0px' }}
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-white placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                    <div className="mx-auto text-center text-sm">
+                        <a
+                        href="./forgot-password"
+                        className="text-center font-medium text-blue-600 hover:text-blue-500"
+                        >
+                        Forgot your password?
+                        </a>
+                    </div>
+                    </div>
+
+                    <div>
+                    <button
+                        type="submit"
+                        onClick={loginUser}
+                        className="flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                    >
+                        Sign in
+                    </button>
+                    </div>
                 </div>
-              </div>
+
+                <div className="mt-6">
+                    <div className="mt-6  gap-3 ">
+                    <div>
+                        <a
+                        style={{ backgroundColor: '#161716', borderWidth: '0px' }}
+                        href="#"
+                        className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                        onClick={loginGoogle}
+                        >
+                        <span className="sr-only">Sign in with Google</span>
+                        <svg
+                            className="h-6 w-6"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 48 48"
+                            width="48px"
+                            height="48px"
+                        >
+                            <path
+                            fill="#FFC107"
+                            d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                            />
+                            <path
+                            fill="#FF3D00"
+                            d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                            />
+                            <path
+                            fill="#4CAF50"
+                            d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                            />
+                            <path
+                            fill="#1976D2"
+                            d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                            />
+                        </svg>
+                        <p className="ml-2">Login with Google</p>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="mt-6">
+                    <div className="mt-6  gap-3 ">
+                    <div>
+                        <a
+                        style={{ backgroundColor: '#161716', borderWidth: '0px' }}
+                        href="#"
+                        className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                        onClick={loginGoogle}
+                        >
+                        <span className="sr-only">Sign in with Google</span>
+                    
+                        <i class="mt-1 mx-2 fas fa-university fa-lg"></i>University Login
+
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
             </div>
-          </div>
+            </div>
         </div>
-      </div>
     </>
   );
 }
