fca6f0ccf3d58144ca3e6007fcc31cf2275079d3
Stephen Stefanatos <stephenstef456@gmail.com>
2023-12-27 03:38:28 -0500

    assignments view for teachers and students connected to backend

--- src/pages/assignments/[code].jsx
+++ src/pages/assignments/[code].jsx
@@ -1,28 +1,96 @@
 import Head from 'next/head';
 import { StandardNav } from '@/components/StandardNav';
 import { Footer } from '@/components/Footer';
-import { ProgressCircle } from "@tremor/react"
+import { ProgressCircle } from '@tremor/react';
 import { MarkdownViewer } from '@/components/MarkdownViewer';
+import { useEffect, useState } from 'react';
 
+const baseUrl = `http://localhost:3001`;
 export default function Slug() {
+  const [assignment, setAssignment] = useState(null);
+  const [flagInput, setFlagInput] = useState('');
+  const [submissions, setSubmissions] = useState([]);
+  const [hints, setHints] = useState([
+    { message: '', penalty: '' },
+    { message: '', penalty: '' },
+    { message: '', penalty: '' },
+  ]);
 
-  const demoAssignments = [
-    {
-      id: 1,
-      title: 'Yummers!',
-      description: 'This is an example assingment on the CTFGuide website. Thos is demonstrating a fun activity hosted on the CTFGuide website.',
-      dueDate: '2023-12-01',
-    },
-    {
-      id: 2,
-      title: 'Assignment 2',
-      description: 'Understanding Cryptography',
-      dueDate: '2024-01-15',
-    },
-    // Add more assignments as needed
-  ];
+  const parseDate = (dateString) => {
+    let dateObject = new Date(dateString);
+    let month = dateObject.getMonth() + 1; // getMonth() returns a zero-based value (where zero indicates the first month of the year)
+    let day = dateObject.getDate();
+    let year = dateObject.getFullYear();
+    let hours = dateObject.getHours();
+    let minutes = dateObject.getMinutes();
+    let ampm = hours >= 12 ? ' PM' : ' AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    let strTime = hours + ':' + minutes + ampm;
+    let formattedDate = `${month}/${day}/${year} ${strTime}`;
+    return formattedDate;
+  };
 
+  const getAssignment = async () => {
+    try {
+      const params = window.location.href.split('/');
+      if (params.length < 5) {
+        return;
+      }
+      const url = `${baseUrl}/classroom-assignments/fetch-assignment/${params[4]}`;
+      const requestOptions = { method: 'GET' };
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      if (data.success) {
+        setAssignment(data.body);
+        await getSubmissions(data.body);
+      } else {
+        console.log(data.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  const getSubmissions = async (assignment) => {
+    try {
+      const url = `${baseUrl}/submission/getSubmissionsForTeachers/${assignment.classroom.id}/${assignment.id}`;
+      const requestOptions = { method: 'GET' };
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      console.log(data);
+      if (data.success) {
+        setSubmissions(data.body);
+      } else {
+        console.log(data.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getAssignment();
+  }, []);
+
+  const checkFlag = () => {
+    if (assignment && flagInput === assignment.solution.keyword) {
+      console.log('You got it!!');
+    } else {
+      console.log('this is incorrect');
+    }
+  };
+
+  const showHint = (i) => {
+    let tmp = [...hints];
+    tmp[i].message = assignment.challenge.hints[i].message;
+    tmp[i].penalty = '(-' + assignment.challenge.hints[i].penalty + ') points';
+    setHints(tmp);
+  };
+
+  console.log(assignment);
+  console.log(submissions);
 
   return (
     <>
@@ -35,133 +103,129 @@ export default function Slug() {
       </Head>
       <StandardNav />
       <div className=" min-h-screen  ">
-
-
-
         <div className="mx-auto mt-4">
+          <a
+            href="/groups/122ctfguide"
+            className="hidden text-neutral-200 hover:text-neutral-500"
+          >
+            <i className="fas fa-long-arrow-alt-left"></i> Return Home
+          </a>
 
-          <a href="/groups/122ctfguide" className='hidden text-neutral-200 hover:text-neutral-500'><i className="fas fa-long-arrow-alt-left"></i> Return Home</a>
+          <div className="w-full bg-gradient-to-r from-blue-800 via-blue-900 to-blue-800 px-4 py-4 ">
+            <div className="mx-auto max-w-6xl">
+              <h1 className="text-3xl font-semibold text-white">
+                {assignment && assignment.name}{' '}
+              </h1>
 
-
-
-          <div className='w-full bg-gradient-to-r from-blue-800 via-blue-900 to-blue-800 px-4 py-4 '>
-
-            <div className='max-w-6xl mx-auto'>
-
-              <h1 className='text-white font-semibold text-3xl'>{demoAssignments[0].title} </h1>
-              <h1 className='text-white'>Due at 11:50 PM 12/25/23</h1>
+              <h1 className="text-white">
+                Due Date: {assignment && parseDate(assignment.dueDate)}{' '}
+              </h1>
             </div>
           </div>
 
-          <div className='max-w-6xl mx-auto mt-4'>
-
-
-            <div className='grid grid-cols-6 gap-x-8 h-full'>
+          <div className="mx-auto mt-4 max-w-6xl">
+            <div className="grid h-full grid-cols-6 gap-x-8">
               <div className="col-span-2">
-                <h1 className='text-white text-xl font-semibold'>Assignment Description</h1>
-                <MarkdownViewer className="text-white" content={demoAssignments[0].description} />
+                <h1 className="text-xl font-semibold text-white">
+                  Assignment Description
+                </h1>
+                <MarkdownViewer
+                  className="text-white"
+                  content={assignment && assignment.description}
+                />
 
-                <b className='text-white'>ASSOCIATED FILES</b>
-                <hr className='bg-neutral-900 rounded-lg border border-blue-600' />
-                <div className='text-white mt-4 px-4 py-1 bg-neutral-800/50 hover:bg-neutral-700/10 cursor-pointer rounded-lg border border-neutral-800/50'>
-                  <h1 className='text-md'><i className="text-white fas fa-file-archive mr-2"></i> this_is_flag.zip</h1>
+                <b className="text-white">ASSOCIATED FILES</b>
+                <hr className="rounded-lg border border-blue-600 bg-neutral-900" />
+                <div className="mt-4 cursor-pointer rounded-lg border border-neutral-800/50 bg-neutral-800/50 px-4 py-1 text-white hover:bg-neutral-700/10">
+                  <h1 className="text-md">
+                    <i className="fas fa-file-archive mr-2 text-white"></i>{' '}
+                    this_is_flag.zip
+                  </h1>
                 </div>
 
-                <p className='font-semibold text-white mt-6'>FLAG SUBMISSION</p>
-                <hr className='bg-neutral-900 rounded-lg border border-blue-600' />
+                <p className="mt-6 font-semibold text-white">FLAG SUBMISSION</p>
+                <hr className="rounded-lg border border-blue-600 bg-neutral-900" />
 
-                <input placeholder='Think you got the flag? Enter it here!' className='text-white w-full mt-4 px-4 py-1 bg-neutral-800/50 hover:bg-neutral-700/10 cursor-pointer rounded-lg border border-neutral-800/50'></input>
-                <button className='mt-3 bg-green-800 hover:bg-green-700 px-2 py-1 text-white rounded-lg'>Check Flag</button>
-
-
-                <p className='font-semibold text-white mt-6'>HINTS</p>
-                <hr className='bg-neutral-900 rounded-lg border border-blue-600' />
-                <div
-
-                  className="mt-3  w-full border-l-2  border-yellow-600  bg-[#212121] px-4 mb-2 text-lg"
-                  enter="transition-opacity duration-75"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="transition-opacity duration-150"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-
+                <input
+                  placeholder="Think you got the flag? Enter it here!"
+                  className="mt-4 w-full cursor-pointer rounded-lg border border-neutral-800/50 bg-neutral-800/50 px-4 py-1 text-white hover:bg-neutral-700/10"
+                  onChange={(e) => setFlagInput(e.target.value)}
+                  value={flagInput}
+                ></input>
+                <button
+                  onClick={checkFlag}
+                  className="mt-3 rounded-lg bg-green-800 px-2 py-1 text-white hover:bg-green-700"
                 >
-                  <p className="text-white">
-                    <span className='text-sm'>Hint 1</span>
-              
-                  </p>
-                </div>
-                <div
+                  Check Flag
+                </button>
 
-className="  w-full border-l-2  border-yellow-600  bg-[#212121] px-4 mb-2 text-lg"
-enter="transition-opacity duration-75"
-enterFrom="opacity-0"
-enterTo="opacity-100"
-leave="transition-opacity duration-150"
-leaveFrom="opacity-100"
-leaveTo="opacity-0"
-
->
-<p className="text-white">
-  <span className='text-sm'>Hint 2</span>
-
-</p>
-</div>
-
-<div
-
-className="  w-full border-l-2  border-yellow-600  bg-[#212121] px-4 mb-2 text-lg"
-enter="transition-opacity duration-75"
-enterFrom="opacity-0"
-enterTo="opacity-100"
-leave="transition-opacity duration-150"
-leaveFrom="opacity-100"
-leaveTo="opacity-0"
-
->
-<p className="text-white">
-  <span className='text-sm'>Hint 3</span>
-
-</p>
-</div>
-
-
-
+                <p className="mt-6 font-semibold text-white">HINTS</p>
+                <hr className="rounded-lg border border-blue-600 bg-neutral-900" />
+                {hints.map((hint, idx) => {
+                  return (
+                    <div
+                      className="mb-2 mt-3 w-full border-l-2 border-yellow-600 bg-[#212121] px-4 text-lg"
+                      enter="transition-opacity duration-75"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="transition-opacity duration-150"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      onClick={() => showHint(idx)}
+                      style={{
+                        cursor: 'pointer',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                      }}
+                    >
+                      <div>
+                        <p className="text-white">
+                          <span className="text-sm">
+                            Hint {idx + 1}: {hint.message}
+                          </span>
+                        </p>
+                      </div>
+                      <span style={{ color: 'white' }}>
+                        -{assignment && assignment.challenge.hints[idx].penalty}{' '}
+                        points
+                      </span>
+                    </div>
+                  );
+                })}
               </div>
 
-              <div className='col-span-4 bg-black h-60 px-4'>
-                <iframe className="w-full h-full" src="https://terminal.ctfguide.com/wetty/ssh/root"></iframe>
-             
-                <p className='font-semibold text-white mt-6'>STUDENT SUBMISSIONS</p>
-                <hr className='bg-neutral-900 rounded-lg border border-blue-600' />
-                <div className='grid grid-cols-4 mt-4 gap-x-2'>
-                  <div className='bg-neutral-800 px-2 py-1 rounded-lg text-white'>
-                    <h1>Pranav Ramesh <i className="ml-2 fas fa-check text-green-500"></i></h1>
-                  </div>
+              <div className="col-span-4 h-60 bg-black px-4">
+                <iframe
+                  className="h-full w-full"
+                  src="https://terminal.ctfguide.com/wetty/ssh/root"
+                ></iframe>
 
-                  <div className='bg-neutral-800 px-2 py-1 rounded-lg text-white'>
-                    <h1>Abhi Byreddy <i className="ml-2 fas fa-check text-green-500"></i></h1>
-                  </div>
-
-                  <div className='bg-neutral-800 px-2 py-1 rounded-lg text-white'>
-                    <h1>Jiaming Wang  <i className="text-red-400 ml-2  fas fa-clock"></i></h1>
-                  </div>
-
-                
+                <p className="mt-6 font-semibold text-white">
+                  STUDENT SUBMISSIONS
+                </p>
+                <hr className="rounded-lg border border-blue-600 bg-neutral-900" />
+                <div className="mt-4 grid grid-cols-3 gap-x-2">
+                  {submissions.length === 0 && 'No students in class...'}
+                  {submissions.map((submission, idx) => (
+                    <div
+                      key={idx}
+                      className="rounded-lg bg-neutral-800 px-2 py-1 text-white"
+                    >
+                      <h1>
+                        {submission.name}{' '}
+                        {submission.submitted ? (
+                          <i className="fas fa-check ml-2 text-green-500"></i>
+                        ) : (
+                          <i className="fas fa-clock  ml-2 text-red-400"></i>
+                        )}
+                      </h1>
+                    </div>
+                  ))}
                 </div>
               </div>
-
-
-
             </div>
           </div>
-
-
-
         </div>
-
-
       </div>
 
       <Footer />
