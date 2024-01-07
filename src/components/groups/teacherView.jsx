874f29f68f40e12b18ca0d2c03891629d1f8229c
AbhiByreddy <54539671+ThunderBird260@users.noreply.github.com>
2024-01-07 01:45:21 -0500

    fixed view all route in teacherview

--- src/components/groups/teacherView.jsx
+++ src/components/groups/teacherView.jsx
@@ -516,7 +516,7 @@ export default function TeacherView({ group }) {
                     </div>
                 )}
                 <button className="text-sm float-right rounded-sm bg-neutral-900  py-1 text-white" onClick = {() => {
-                  window.location.href = `../groups/view-all-assignments`
+                  window.location.href = `../${classroom.classCode}/view-all-assignments`
                 }}>
                   
                   <i className="fas fa-external-link-alt"></i> View All
