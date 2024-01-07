94d7fedbb73d8f382692ef466209387f673de74b
AbhiByreddy <54539671+ThunderBird260@users.noreply.github.com>
2024-01-07 00:24:05 -0500

    Updated teacherview and viewallassignments
    added no assignments box on assignment view ONLY IN TEACHER VIEW, updated on view all assignments tab, fixed some grammar errors'

--- src/pages/groups/[group]/view-all-assignments.jsx
+++ src/pages/groups/[group]/view-all-assignments.jsx
@@ -41,9 +41,9 @@ const ViewAllAssignments = () => {
             </Head>
             <StandardNav />
             <div className='max-w-6xl mx-auto grid grid-cols-3 gap-x-8 flex justify-center mt-6'>
-            <h1 className="text-2xl font-semibold mx-auto text-white">Assignments</h1>
+                <h1 className="text-2xl font-semibold mx-auto text-white">Assignments</h1>
             </div>
-            
+
 
             <div className='max-w-6xl mx-auto grid grid-cols-3 gap-x-8 flex justify-center mt-10'>
                 <div className='text-white'>Collumn 1</div>
@@ -63,6 +63,49 @@ const ViewAllAssignments = () => {
                                     className="mb-2 border-l-4 border-gray-600 cursor-pointer rounded-sm bg-neutral-800/50 px-3 py-3 hover:bg-neutral-800"
                                 >
                                     {/* Make assignment box look pretty */}
+                                    <h2 className="text-md text-white">
+                                        <Tooltip id="quiz-tooltip" place="left" />
+                                        <Tooltip id="test-tooltip" place="left" />
+                                        <Tooltip id="homework-tooltip" place="left" />
+                                        <Tooltip id="assessment-tooltip" place="left" />
+
+                                        {assignment.category === 'quiz' && (
+                                            <i
+                                                title="quiz"
+                                                className="fas fa-question-circle"
+                                                data-tooltip-id="quiz-tooltip"
+                                                data-tooltip-content="Quiz"
+                                            ></i>
+                                        )}
+                                        {assignment.category === 'test' && (
+                                            <i
+                                                title="test"
+                                                className="fas fa-clipboard-check"
+                                                data-tooltip-id="test-tooltip"
+                                                data-tooltip-content="Test"
+                                            ></i>
+                                        )}
+                                        {assignment.category === 'homework' && (
+                                            <i
+                                                title="homework"
+                                                className="fas fa-book"
+                                                data-tooltip-id="homework-tooltip"
+                                                data-tooltip-content="Homework"
+                                            ></i>
+                                        )}
+                                        {assignment.category === 'assessment' && (
+                                            <i
+                                                title="assessment"
+                                                className="fas fa-file-alt"
+                                                data-tooltip-id="assessment-tooltip"
+                                                data-tooltip-content="Assessment"
+                                            ></i>
+                                        )}
+
+                                        <span className="ml-0.5"> {assignment.name} </span>
+                                    </h2>
+                                    <p className="text-white">Due: {parseDate(assignment.dueDate)}  |  0/{assignment.points}pts</p>
+
                                 </div>
                             ))}
                 </div>
@@ -75,6 +118,7 @@ const ViewAllAssignments = () => {
 
                 <div className='mt-1'>
                     <h2 className="text-lg font-semibold text-white mb-4">Past Assignments</h2>
+                    {/*Make so that subitted assignments are also here*/}
                     {classroom &&
                         classroom.assignments &&
                         classroom.assignments
@@ -88,6 +132,50 @@ const ViewAllAssignments = () => {
                                     className="mb-2 border-l-4 border-green-600 cursor-pointer rounded-sm bg-neutral-800/50 px-3 py-3 hover:bg-neutral-800"
                                 >
                                     {/* Make assignment look pretty*/}
+                                    <h2 className="text-md text-white">
+                                        <Tooltip id="quiz-tooltip" place="left" />
+                                        <Tooltip id="test-tooltip" place="left" />
+                                        <Tooltip id="homework-tooltip" place="left" />
+                                        <Tooltip id="assessment-tooltip" place="left" />
+
+                                        {assignment.category === 'quiz' && (
+                                            <i
+                                                title="quiz"
+                                                className="fas fa-question-circle"
+                                                data-tooltip-id="quiz-tooltip"
+                                                data-tooltip-content="Quiz"
+                                            ></i>
+                                        )}
+                                        {assignment.category === 'test' && (
+                                            <i
+                                                title="test"
+                                                className="fas fa-clipboard-check"
+                                                data-tooltip-id="test-tooltip"
+                                                data-tooltip-content="Test"
+                                            ></i>
+                                        )}
+                                        {assignment.category === 'homework' && (
+                                            <i
+                                                title="homework"
+                                                className="fas fa-book"
+                                                data-tooltip-id="homework-tooltip"
+                                                data-tooltip-content="Homework"
+                                            ></i>
+                                        )}
+                                        {assignment.category === 'assessment' && (
+                                            <i
+                                                title="assessment"
+                                                className="fas fa-file-alt"
+                                                data-tooltip-id="assessment-tooltip"
+                                                data-tooltip-content="Assessment"
+                                            ></i>
+                                        )}
+
+                                        <span className="ml-0.5"> {assignment.name} </span>
+                                    </h2>
+                                    <p className="text-white">Due: {parseDate(assignment.dueDate)}  |  0/{assignment.points}pts</p>
+
+
                                 </div>
                             ))}
                 </div>
