24fc58fe66f3271b60e95f3ac563ef69dc86cd12
AbhiByreddy <54539671+ThunderBird260@users.noreply.github.com>
2024-01-19 13:27:21 -0500

    stephens poopy face

--- src/components/StandardNav.jsx
+++ src/components/StandardNav.jsx
@@ -196,7 +196,7 @@ export function StandardNav() {
                     </Disclosure.Button>
                   </div>
                   <div className="flex flex-shrink-0 items-center">
-                    <Link href="../dashboard" aria-label="Dashboard">
+                    <Link href={`${baseUrl}`} aria-label="Dashboard">
                       {isAdmin ? <LogoAdmin /> : <Logo />}
                     </Link>
                   </div>
