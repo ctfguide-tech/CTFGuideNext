d937375ebda175b6e33b44d86cf29d64e6774aed
AbhiByreddy <54539671+ThunderBird260@users.noreply.github.com>
2024-03-01 19:30:34 -0500

    kana in settings for class

--- src/pages/groups/[group]/settings.jsx
+++ src/pages/groups/[group]/settings.jsx
@@ -18,12 +18,12 @@ const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 const frontend_baselink = process.env.NEXT_PUBLIC_FRONTEND_URL;
 
 const defaultImages = [
-  'https://robohash.org/pranavramesh',
-  'https://robohash.org/laphatize',
-  'https://robohash.org/stevewilkers',
-  'https://robohash.org/rickast',
-  'https://robohash.org/picoarc',
-  'https://robohash.org/jasoncalcanis',
+  '/DefaultKana.png',
+  '/CuteKana.png',
+  '/FancyKana.png',
+  '/ConfusedKana.png',
+  '/TophatKana.png',
+  
 ];
 
 export default function teacherSettings() {
