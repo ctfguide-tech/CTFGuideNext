17183274dbfc59a2defcc00f59a57c8076d7fee0
kynhix <kristopher.t.hicks@gmail.com>
2024-04-15 20:29:44 -0400

    Updated challenge pages
    Placeholder alerts on submitting flag
    Updated style
    Properly fetches data
    Updated challenge card style

--- src/components/profile/ChallengeCard.jsx
+++ src/components/profile/ChallengeCard.jsx
@@ -9,7 +9,6 @@ import Skeleton from 'react-loading-skeleton';
 const ChallengeCard = (_props) => {
   const { challenge, ...props } = _props;
   const baseUrl = process.env.NEXT_PUBLIC_FRONTEND_URL;
-  console.log(challenge)
   const dateFormatted = (date) =>
     new Date(date)
       .toLocaleDateString('en-US', {
@@ -37,26 +36,26 @@ const ChallengeCard = (_props) => {
     challenge && (
       <Link {...props} className={`group w-full rounded-sm card-container transition-all shadow-sm hover:shadow-lg shadow-black/40 ${props.className ?? ''}`} href={`${baseUrl}/challenges/${challenge.id}`} >
         <CardDecorator position='left' className={`${colorBG[challenge.difficulty || 0]} w-2 transition-colors`}></CardDecorator>
-        <div className="bg-neutral-700 group-hover:bg-stone-600 transition-colors pl-8 pr-6 py-4 box-content border-y border-r border-neutral-100/20 rounded-l-lg text-sm leading-8 text-gray-300">
+        <div className="bg-neutral-800 group-hover:bg-stone-600 transition-colors pl-8 pr-6 py-4 box-content border-y border-r border-neutral-100/20 rounded-l-lg text-sm leading-8 text-gray-300">
           <h1 className="text-2xl font-semibold text-white">{challenge.title}</h1>
           <h1 className="text-base text-neutral-400 line-clamp-1">{challenge.creator}</h1>
           <h1 className={`text-base px-2 mb-1 leading-6 font-bold capitalize w-fit rounded-sm text-neutral-50 ${colorText[challenge.difficulty || 0]}`}>{challenge.difficulty?.toLowerCase() || <Skeleton />}</h1>
           <div className="flex justify-between">
             <p className="text-neutral-400 flex">
-              <i class="text-lg mt-[5px] mr-2 fas fa-solid fa-calendar"></i>
+              <i className="text-lg mt-[5px] mr-2 fas fa-solid fa-calendar"></i>
               {dateFormatted(challenge.createdAt)}
             </p>
             <p className="flex text-neutral-200 opacity-70 items-center text-sm">
-              <i class="fas fa-solid fa-eye mr-2 text-lg"></i>
+              <i className="fas fa-solid fa-eye mr-2 text-lg"></i>
               {challenge.views}
-              <i class="ml-4 mr-2 text-neutral-300 fas fa-solid fa-heart text-lg"></i>
+              <i className="ml-4 mr-2 text-neutral-300 fas fa-solid fa-heart text-lg"></i>
               {challenge.upvotes}
             </p>
           </div>
         </div>
       </Link >)
     || (
-      <div className={`bg-neutral-700 w-full rounded-sm pl-8 pr-6 py-4 leading-8 ${props.className ?? ''}`}>
+      <div {...props} className={`bg-neutral-800 w-full rounded-sm pl-8 pr-6 py-4 leading-8 ${props.className ?? ''}`}>
         <Skeleton baseColor='#999' highlightColor='#eee' width='10rem' />
         <Skeleton baseColor='#999' highlightColor='#eee' width='12rem' />
         <Skeleton baseColor='#999' highlightColor='#eee' width='4rem' />
--- src/pages/challenges/[...id].jsx
+++ src/pages/challenges/[...id].jsx
@@ -1,22 +1,78 @@
+import { MarkdownViewer } from "@/components/MarkdownViewer";
 import { StandardNav } from "@/components/StandardNav";
+import request from "@/utils/request";
 import { DocumentTextIcon } from "@heroicons/react/20/solid";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState, lazy, useMemo, memo } from "react";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css';
+import ReactMarkdown from "react-markdown";
 
 export default function Challenge() {
   const router = useRouter();
-  if (!router.isReady) {
-    return <div></div>
-  }
-  const [urlChallengeId, urlSelectedTab] = router.query.id;
+  // I hate this
+  const [urlChallengeId, urlSelectedTab] = (router ?? {})?.query?.id ?? [undefined, undefined];
 
+  // Tab system is designed to keep browser state in url,
+  // while mainting persistence of the terminal.
   const tabs = {
-    'description': { text: 'Description', element: DescriptionPage },
-    'write-up': { text: 'Write Up', element: WriteUpPage },
+    'description': { text: 'Description', element: DescriptionPage, },
+    'write-up': { text: 'Write Up', element: WriteUpPage, },
   }
-
   const selectedTab = tabs[urlSelectedTab] ?? tabs.description;
+  const [challenge, setChallenge] = useState(null);
+
+  useEffect(() => {
+    if (!urlChallengeId) {
+      return;
+    }
+    (async () => {
+      try {
+        const getChallengeByIdEndPoint = `${process.env.NEXT_PUBLIC_API_URL}/challenges/${urlChallengeId}`;
+        const getChallengeResult = await request(getChallengeByIdEndPoint, "GET", null);
+        if (getChallengeResult.success) {
+          setChallenge(getChallengeResult.body);
+        }
+      } catch (error) { console.error(error) }
+    })();
+  }, [urlChallengeId]);
+
+  const [loadingFlagSubmit, setLoadingFlagSubmit] = useState(false);
+  const onSubmitFlag = (e) => {
+    e.preventDefault();
+    if (loadingFlagSubmit) {
+      return;
+    }
+    const formElements = e.target.elements;
+    const flag = formElements.flag.value;
+    setLoadingFlagSubmit(true);
+    (async () => {
+      try {
+        const submitChallengeEndpoint = `${process.env.NEXT_PUBLIC_API_URL}/challenges/${urlChallengeId}/submissions`;
+        const submitChallengeResult = await request(submitChallengeEndpoint, 'POST', { keyword: flag });
+        console.log(submitChallengeResult)
+        const { success, incorrect, error } = submitChallengeResult ?? {};
+        if (error || !submitChallengeResult) {
+          // An error occurred >:(
+          alert(error);
+          return;
+        }
+        if (incorrect) {
+          // Incorrect
+          alert(incorrect);
+          return;
+        }
+        // Success
+        alert(success);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoadingFlagSubmit(false);
+      }
+    })();
+  }
 
   return (
     <>
@@ -38,17 +94,25 @@ export default function Challenge() {
             <div className="flex shrink-0 p-1 items-center bg-neutral-700 h-12 w-full">
               {Object.entries(tabs).map(([url, tab]) => <TabLink tabName={tab.text} selected={urlSelectedTab == url} url={`/challenges/${urlChallengeId}/${url}`} key={url} />)}
             </div>
-            {<selectedTab.element />}
+            {/* Only this element should rerender on tab switch */}
+            {<selectedTab.element challenge={challenge} />}
           </div>
           <div className="flex flex-col flex-1 bg-neutral-800 overflow-hidden rounded-md">
-            <div className="grow bg-neutral-950 w-full"></div>
-            <div className="shrink-0 bg-neutral-800 h-10 w-full">Submit Flag</div>
+            <div className="grow bg-neutral-950 w-full">
+            </div>
+            <div className="shrink-0 bg-neutral-800 h-12 w-full">
+              <form action="" method="get" onSubmit={onSubmitFlag} className="flex p-1 gap-2 h-full">
+                <input name="flag" type="text" required placeholder="Flag..." className="text-black h-full p-0 rounded-sm grow px-2 w-1/2" />
+                <input name="submitFlag" type="submit" value="Submit Flag" disabled={loadingFlagSubmit} className="h-full border border-green-500/50 hover:border-green-200/50 bg-green-600 hover:bg-green-500 disabled:bg-neutral-800 disabled:text-neutral-400 disabled:border-neutral-500/50 transition-all text-green-50 cursor-pointer disabled:cursor-default px-2 rounded-sm" />
+              </form>
+            </div>
           </div>
         </main >
       </div>
     </>
   )
 }
+
 
 function TabLink({ tabName, selected, url }) {
   return (
@@ -59,117 +123,50 @@ function TabLink({ tabName, selected, url }) {
   )
 }
 
-function DescriptionPage() {
+function DescriptionPage({ challenge }) {
+  const colorText = {
+    'BEGINNER': 'bg-blue-500 text-blue-50',
+    'EASY': 'bg-green-500 text-green-50',
+    'MEDIUM': 'bg-orange-500 text-orange-50',
+    'HARD': 'bg-red-500 text-red-50',
+    'INSANE': 'bg-purple-500 text-purple-50',
+  };
   return (
     <>
       <div className="grow bg-neutral-800 text-gray-50 p-3 overflow-y-auto">
-        <h2 className="text-2xl font-semibold pt-2">Title of the challenge</h2>
-        <p>Creator (tags tags tags)</p>
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-        This is scrollable content.<br />
-      </div>
+        <h1 className="text-4xl font-semibold py-2 line-clamp-1">
+          {challenge ? challenge.title : <Skeleton baseColor="#333" highlightColor="#666" />}
+        </h1>
+        <h2 className="flex flex-wrap gap-2 pb-2">
+          {challenge ? (
+            <>
+              {<Tag bgColor={colorText[challenge.difficulty]} textColor="font-bold">{challenge.difficulty.toLowerCase()}</Tag>}
+              {challenge.category.map((s) => <Tag key={s}>{s}</Tag>)}
+            </>)
+            : <Skeleton baseColor="#333" highlightColor="#666" />}
+        </h2>
+        <h2 className="flex gap-2 pb-8">
+          {challenge ? (
+            <>
+              <Link href={`/users/${challenge.creator}`} className="text-blue-500 pr-3 hover:underline">{challenge.creator} </Link>
+              <p className="flex text-neutral-200 opacity-70 items-center text-sm">
+                <i className="fas fa-solid fa-eye mr-2 text-lg"></i>
+                {challenge.views}
+                <i className="ml-4 mr-2 text-neutral-300 fas fa-solid fa-heart text-lg"></i>
+                {challenge.upvotes}
+              </p>
+            </>)
+            : <Skeleton baseColor="#333" highlightColor="#666" />}
+        </h2>
+        {challenge ? <MarkdownViewer content={challenge.content}></MarkdownViewer> : <Skeleton baseColor="#333" highlightColor="#666" count={8} />}
+      </div >
       <div className="shrink-0 bg-neutral-800 h-10 w-full"></div>
     </>
   )
+}
+
+function Tag({ bgColor = 'bg-neutral-700', textColor = 'text-neutral-50', children }) {
+  return <p className={`${bgColor} ${textColor} capitalize rounded-sm px-2`}>{children}</p>
 }
 
 function WriteUpPage() {
