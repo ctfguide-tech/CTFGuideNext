e0f5f41de4773570c1d141b03661b32886eda4d3
Pranav Ramesh <laphatize@protonmail.com>
2024-05-16 15:29:40 -0400

    feat: components + more responsiveness

--- src/components/StandardNav.jsx
+++ src/components/StandardNav.jsx
@@ -26,8 +26,8 @@ import 'reactjs-popup/dist/index.css';
 import { useRouter } from 'next/router';
 import { LogoAdmin } from './LogoAdmin';
 import { CSSTransition } from 'react-transition-group';
-
-
+import SearchModal from './nav/SearchModal';
+import SpawnTerminal from './nav/SpawnTerminal';
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
@@ -46,6 +46,8 @@ const DEFAULT_NOTIFICATION = {
 };
 
 export function StandardNav({ guestAllowed, alignCenter = true }) {
+  const [terminaIsOpen, setTerminalIsOpen] = useState(false);
+
   const [isAdmin, setIsAdmin] = useState(false);
   const [points, setPoints] = useState('0');
   const [notifications, setNotifications] = useState([]);
@@ -229,125 +231,9 @@ export function StandardNav({ guestAllowed, alignCenter = true }) {
             </div>
     )}
 
-<Transition.Root show={open} as={Fragment}>
-      <Dialog className="relative z-10" onClose={setOpen}>
-        <div className="fixed inset-0" />
-
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                  <div className="flex h-full flex-col divide-y divide-neutral-800 bg-neutral-900 border-l-2 border-neutral-800 shadow-xl">
-                    <div className="flex min-h-0 flex-1 flex-col overflow-y-scroll py-6">
-                      <div className="px-4 sm:px-6">
-                        <div className="flex items-start justify-between">
-                          <Dialog.Title className="text-base font-semibold leading-6 text-xl text-white">
-                          <i className="fas fa-terminal"></i> Spawn a terminal
-                          </Dialog.Title>
-                          <div className="ml-3 flex h-7 items-center">
-                            <button
-                              type="button"
-                              className="relative rounded-md bg-red-900 text-red-400 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-500"
-                              onClick={() => setOpen()}
-                            >
-                              <span className="absolute -inset-2.5" />
-                              <span className="sr-only">Close panel</span>
-                              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="relative mt-6 flex-1 px-4 sm:px-6 text-white">
-
-                      <h1>Choose an operating system</h1>
-                      <div>
-
-      <select
-        id="os"
-        name="os"
-        className="mt-2 block w-full bg-neutral-800 rounded-md py-1.5 pl-3 pr-10 text-white  border-neutral-800 sm:text-sm sm:leading-6"
-        defaultValue="Ubuntu 22.10 LTS"
-      >
-        <option>Ubuntu 22.10 LTS</option>
-        <option>Alpine Linux</option>
-        <option>Kali Linux (CTFGuide Pro)</option>
-      </select>
-    </div>
-
-    <div className='border-l-4  border-blue-600 px-4 py-3 bg-neutral-800 mt-4'>
-              <h1>You need CTFGuide Pro to use this operating system.</h1>
-              <button className='text-sm bg-blue-600 hover:bg-blue-500 px-2 py-1 mt-2 rounded-lg '>Upgrade now for $5/month</button>
-    </div>
-
-    <h1 className="mt-6">Container Interaction</h1>
-
-    <div className="mt-2 flex w-full mx-auto text-center gap-x-4">
-      <div className='border rounded-lg w-full py-4 cursor-pointer hover:bg-neutral-600/50'>
-              <h1>WebVNC</h1>
-      </div>
-           <div className='border rounded-lg w-full py-4 cursor-pointer hover:bg-neutral-600/50'>
-              <h1>ShellInABox</h1>
-      </div>
-    </div>
-
-    <h1 className="mt-6">Import Files</h1>
-    <div className="mt-2 flex justify-center rounded-lg border border-dashed border-white/25 px-6 py-10">
-                <div className="text-center">
-                <i className="text-4xl text-neutral-200 fas fa-file-archive"></i>
-                  <div className="mt-4 flex text-sm leading-6 text-gray-400">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-blue-500"
-                    >
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                    </label>
-                    <p className="pl-1">or drag and drop</p>
-                  </div>
-                  <p className="text-xs leading-5 text-gray-400">.zip</p>
-                </div>
-              </div>
-     
-      
 
 
-                      </div>
-                    </div>
-                    <div className="flex flex-shrink-0 justify-end px-4 py-4">
-                      <button
-                        type="button"
-                        className="rounded-md  px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400"
-                        onClick={() => {
-                          setOpen(true);
-                        
-                        }}
-                      >
-                        Cancel
-                      </button>
-                      <button
-                        type="submit"
-                        className="ml-4 inline-flex justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
-                      >
-                        Deploy machine
-                      </button>
-                    </div>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+
       <Disclosure as="nav" className=" shadow border-b border-neutral-800">
         {({ open }) => (
           <>
@@ -398,20 +284,20 @@ export function StandardNav({ guestAllowed, alignCenter = true }) {
                     </Link>
                     <Link
                       href='/create'
-                      className={linkClass('/create') + " lg:hidden"}
+                      className={linkClass('/create') + " hidden md:hidden lg:hidden xl:hidden 2xl:inline-flex"}
                     >
                       Create
                                          </Link>
 
                     <Link
                       href='/groups'
-                      className={linkClass('/groups')}
+                      className={linkClass('/groups') + " hidden md:hidden lg:hidden xl:hidden 2xl:inline-flex"}
                     >
                       Classrooms
                     </Link>
 
                     {/* Ellipsis dropdown */}
-                    <Popover className="relative lg:hidden ">
+                    <Popover className="relative  md:block lg:block xl:block 2xl:hidden">
                       {({ open }) => (
                         <>
                           <Popover.Button className="inline-flex items-center p-2 text-gray-400 hover:text-white ring-none outline-none " >
@@ -454,17 +340,16 @@ export function StandardNav({ guestAllowed, alignCenter = true }) {
                     </Popover>
 
                       {/*search bar*/}
-                      <div className="mt-3 ml-4 flex-grow ">
-                      
-                          <div
-                          type="text" 
-                          className="w-full px-16 vertical-align flex  py-2 text-sm font-semibold text-neutral-500 hover:text-neutral-50 placeholder-gray-300 bg-neutral-800   hover:cursor-pointer border border-transparent rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                          onClick={() => setShowSearchModal(true)}
-                        >
-                          <FontAwesomeIcon icon={faSearch} className="w-3 h-3 mt-1.5 mr-1" /> Search for anything
-                          </div>
-                        
-                        </div>
+                      <div className="mt-3 ml-4 flex-grow">
+      <div
+        type="text"
+        className="w-full px-16 py-2 text-sm font-semibold text-neutral-500 hover:text-neutral-50 placeholder-gray-300 bg-neutral-800 hover:cursor-pointer border border-transparent rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent flex items-center"
+        onClick={() => setShowSearchModal(true)}
+      >
+        <FontAwesomeIcon icon={faSearch} className="w-3 h-3 md:mt-1.5 mr-1" />
+        <span className="hidden md:inline">Search for anything</span>
+      </div>
+    </div>
                     {/* <Link */}
                     {/*   href={`${baseUrl}/live`} */}
                     {/*   className="inline-flex items-center border-b-2 border-transparent px-4 pt-1 text-sm font-semibold text-gray-300 hover:text-gray-50 transition-all" */}
@@ -490,7 +375,9 @@ export function StandardNav({ guestAllowed, alignCenter = true }) {
                 </div>
                 {!guestAllowed &&
                   <div className="flex items-center ">
-                    <button className='bg-blue-600 hover:bg-blue-500 text-white px-2 py-1 text-sm rounded-md' onClick={setOpen(true)}>Open Terminal</button>
+                    <button className='bg-blue-600 hover:bg-blue-500 text-white px-2 py-1 text-sm rounded-md'
+                            onClick={() => setTerminalIsOpen(true)}
+                            >Open Terminal</button>
                    <div  className="ml-2 mb-0 flex items-center space-x-2 rounded-lg px-4 py-1" 
                      style={{ backgroundColor: '#212121', borderWidth: '0px' }} 
                      > 
@@ -673,61 +560,10 @@ export function StandardNav({ guestAllowed, alignCenter = true }) {
         )}
       </Disclosure >
 
-      {showSearchModal && (
-          <div
-          onEnter={() => setShowSearchModal(true)}
-      
-        >
-        <div 
-      className="fastanimate  fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70 animate__animated animate__faster animate__fadeIn"
-      style={{ 
-        backdropFilter: 'blur(2px)',
-       }}
-      onClick={() => setShowSearchModal(false)}
-    >
-         <div 
-        className="relative transform overflow-hidden rounded-lg bg-neutral-800 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:p-6 sm:max-w-3xl sm:mx-auto sm:rounded-xl sm:shadow-2xl sm:ring-1 sm:ring-white/10"
-        onClick={(e) => e.stopPropagation()}
-      >
-            <div className="px-4 py-5 sm:px-6 mx-auto">
-              <div className='flex items-center mx-auto'>
-                <FontAwesomeIcon icon={faSearch} className="w-5 h-5  mr-1 text-white" />
-                <input placeholder="Search for challenges, users, or competitions" className='w-full border-0 text-xl focus:ring-0 bg-transparent text-white' autoFocus></input>
-              </div>
-              <h1 className='mt-10 text-xl text-white font-semibold mb-4'>Search by Category</h1>
-              <div className="flex flex-wrap gap-2">
-                <button className="px-4 py-2 rounded bg-blue-500 bg-opacity-50 border border-blue-800 hover:brightness-110 text-white flex items-center gap-2">
-                  <FontAwesomeIcon icon={faBug} className='w-4 h-4' />
-                  <span>Web Exploitation</span>
-                </button>
-                <button className="px-4 py-2 rounded bg-green-500 bg-opacity-50 border border-green-800 hover:brightness-110 text-white flex items-center gap-2">
-                  <FontAwesomeIcon icon={faLock} className='w-4 h-4' />
-                  <span>Cryptography</span>
-                </button>
-                <button className="px-4 py-2 rounded bg-red-500 bg-opacity-50 border border-red-800 hover:brightness-110 text-white flex items-center gap-2">
-                  <FontAwesomeIcon icon={faUserSecret} className='w-4 h-4' />
-                  <span>Reverse Engineering</span>
-                </button>
-                <button className="px-4 py-2 rounded bg-yellow-500 bg-opacity-50 border border-yellow-800  hover:brightness-110 text-white flex items-center gap-2">
-                  <FontAwesomeIcon icon={faNetworkWired} className='w-4 h-4' />
-                  <span>Networking</span>
-                </button>
-                <button className="px-4 py-2 rounded bg-purple-500 bg-opacity-50 border border-purple-800 hover:brightness-110  text-white flex items-center gap-2">
-                  <FontAwesomeIcon icon={faBrain} className='w-4 h-4' />
-                  <span>Forensics</span>
-                </button>
-                <button className="px-4 py-2 rounded bg-pink-500 bg-opacity-50 border border-pink-800 hover:brightness-110  text-white flex items-center gap-2">
-                  <FontAwesomeIcon icon={faTerminal} className='w-4 h-4' />
-                  <span>Binary Exploitation</span>
-                </button>
-              </div>
-            </div>
-            <div className="px-4 py-5 sm:p-6">{/* Content goes here */}</div>
-          </div>
-        </div>
-        </div>
-      )}
 
+             <SearchModal showSearchModal={showSearchModal} setShowSearchModal={setShowSearchModal} />
+
+ 
       {isAdmin && (
         <div className="bg-neutral-800 py-1 text-center text-sm text-white ">
           <h1>CTFGuide is running in development mode. </h1>
@@ -742,6 +578,9 @@ export function StandardNav({ guestAllowed, alignCenter = true }) {
           </div>
         )
       }
+
+
+      <SpawnTerminal open={terminaIsOpen} setOpen={setTerminalIsOpen} />
     </>
   );
 }
---
+++ src/components/nav/SpawnTerminal.jsx
@@ -0,0 +1,121 @@
+import React, { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+
+const SpawnTerminal = ({ open, setOpen }) => {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog className="relative z-10" onClose={setOpen}>
+        <div className="fixed inset-0" />
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                  <div className="flex h-full flex-col divide-y divide-neutral-800 bg-neutral-900 border-l-2 border-neutral-800 shadow-xl">
+                    <div className="flex min-h-0 flex-1 flex-col overflow-y-scroll py-6">
+                      <div className="px-4 sm:px-6">
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title className="text-base font-semibold leading-6 text-xl text-white">
+                            <i className="fas fa-terminal"></i> Spawn a terminal
+                          </Dialog.Title>
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="relative rounded-md bg-red-900 text-red-400 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-500"
+                              onClick={() => setOpen(false)}
+                            >
+                              <span className="absolute -inset-2.5" />
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="relative mt-6 flex-1 px-4 sm:px-6 text-white">
+                        <h1>Choose an operating system</h1>
+                        <div>
+                          <select
+                            id="os"
+                            name="os"
+                            className="mt-2 block w-full bg-neutral-800 rounded-md py-1.5 pl-3 pr-10 text-white border-neutral-800 sm:text-sm sm:leading-6"
+                            defaultValue="Ubuntu 22.10 LTS"
+                          >
+                            <option>Ubuntu 22.10 LTS</option>
+                            <option>Alpine Linux</option>
+                            <option>Kali Linux (CTFGuide Pro)</option>
+                          </select>
+                        </div>
+
+                        <div className='border-l-4 border-blue-600 px-4 py-3 bg-neutral-800 mt-4'>
+                          <h1>You need CTFGuide Pro to use this operating system.</h1>
+                          <button className='text-sm bg-blue-600 hover:bg-blue-500 px-2 py-1 mt-2 rounded-lg'>
+                            Upgrade now for $5/month
+                          </button>
+                        </div>
+
+                        <h1 className="mt-6">Container Interaction</h1>
+                        <div className="mt-2 flex w-full mx-auto text-center gap-x-4">
+                          <div className='border rounded-lg w-full py-4 cursor-pointer hover:bg-neutral-600/50'>
+                            <h1>WebVNC</h1>
+                          </div>
+                          <div className='border rounded-lg w-full py-4 cursor-pointer hover:bg-neutral-600/50'>
+                            <h1>ShellInABox</h1>
+                          </div>
+                        </div>
+
+                        <h1 className="mt-6">Import Files</h1>
+                        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-white/25 px-6 py-10">
+                          <div className="text-center">
+                            <i className="text-4xl text-neutral-200 fas fa-file-archive"></i>
+                            <div className="mt-4 flex text-sm leading-6 text-gray-400">
+                              <label
+                                htmlFor="file-upload"
+                                className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-blue-500"
+                              >
+                                <span>Upload a file</span>
+                                <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                              </label>
+                              <p className="pl-1">or drag and drop</p>
+                            </div>
+                            <p className="text-xs leading-5 text-gray-400">.zip</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-shrink-0 justify-end px-4 py-4">
+                      <button
+                        type="button"
+                        className="rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400"
+                        onClick={() => setOpen(false)}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="submit"
+                        className="ml-4 inline-flex justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+                      >
+                        Deploy machine
+                      </button>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default SpawnTerminal;
