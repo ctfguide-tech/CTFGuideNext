c7b3b32c325e05bd86b45f9f250bc96b4c455d6e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-06-27 11:07:48 +0530

    Drop down menu

--- src/components/settingComponents/sidebar.jsx
+++ src/components/settingComponents/sidebar.jsx
@@ -1,41 +1,82 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export default function Sidebar() {
-  const router = useRouter()
+  //useState and toggle function
+  const [isopen, setOpen] = useState(true);
+  const toggleMenu = () => setOpen(!isopen);
+
+  const router = useRouter();
   return (
-    <div
-      className="  mt-10 flex-none border-r pl-10 pr-10 text-gray-900"
-      style={{ borderColor: '#212121' }}
-    >
-      <ul className="mr-2 py-2">
-        <li className="py-1">
-          <button className="px-2 py-1  text-lg font-medium text-white hover:text-gray-400"
-            onClick={() => router.push('../settings', null, { shallow: true })}>
-            General
-          </button>
-        </li>
-        <li className="py-1 ">
+    <div>
+      <button onClick={toggleMenu} className="  p-3 text-white sm:hidden">
+        <svg
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="h-6 w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
 
-          <button className="px-2 py-1  text-lg font-medium text-white hover:text-gray-400"
-            onClick={() => router.push('../settings/security', null, { shallow: true })}>
-            Security
-          </button>
-        </li>
-        <li className="py-1 ">
-          <button className="px-2 py-1  text-lg font-medium text-white hover:text-gray-400"
-            onClick={() => router.push('../settings/preferences', null, { shallow: true })}>
-            Email Preferences
-          </button>
-
-        </li>
-        <li className="py-1 ">
-          <button className="px-2 py-1  text-lg font-medium text-white hover:text-gray-400"
-            onClick={() => router.push('../settings/billing', null, { shallow: true })}>
-            Billing
-          </button>
-        </li>
-      </ul>
+      {/* Menu */}
+      {isopen && (
+        <div
+          className={`mt-10 block flex-none border-r pl-10 pr-10 text-gray-900 `}
+          style={{ borderColor: '#212121' }}
+        >
+          <ul className="mr-2 py-2">
+            <li className="py-1">
+              <button
+                className="px-2 py-1  text-lg font-medium text-white hover:text-gray-400"
+                onClick={() =>
+                  router.push('../settings', null, { shallow: true })
+                }
+              >
+                General
+              </button>
+            </li>
+            <li className="py-1 ">
+              <button
+                className="px-2 py-1  text-lg font-medium text-white hover:text-gray-400"
+                onClick={() =>
+                  router.push('../settings/security', null, { shallow: true })
+                }
+              >
+                Security
+              </button>
+            </li>
+            <li className="py-1 ">
+              <button
+                className="px-2 py-1 text-lg font-medium text-white hover:text-gray-400"
+                onClick={() =>
+                  router.push('../settings/preferences', null, {
+                    shallow: true,
+                  })
+                }
+              >
+                Preferences
+              </button>
+            </li>
+            <li className="py-1 ">
+              <button
+                className="px-2 py-1  text-lg font-medium text-white hover:text-gray-400"
+                onClick={() =>
+                  router.push('../settings/billing', null, { shallow: true })
+                }
+              >
+                Billing
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
