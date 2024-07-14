bb3ee2a448201c815b72e636fdbe6489ee565afa
Pranav Ramesh <laphatize@protonmail.com>
2024-07-14 10:36:10 -0400

    feat: layout updates for user profile

--- src/pages/users/v2/[user].jsx
+++ src/pages/users/v2/[user].jsx
@@ -29,13 +29,13 @@ export default function Create() {
             case 'CREATED CHALLENGES':
                 return <>
                     <h1 className='text-2xl font-bold text-neutral-400'>CREATED CHALLENGES</h1>
-                    <CreatedChallenges/>
-                    </>
+                    <CreatedChallenges />
+                </>
             case 'BADGES':
                 return <>
                     <h1 className='text-2xl font-bold text-neutral-400'>BADGES</h1>
-                    <Badges/>
-                    </>
+                    <Badges />
+                </>
             default:
                 return null;
         }
@@ -65,23 +65,23 @@ export default function Create() {
 
             <main className='max-w-7xl mx-auto mt-10'>
 
-                <div className='text-white text-xl px-10  py-10 w-full bg-neutral-800 flex items-center'>
+                <div className='text-white text-xl px-10 py-10 w-full bg-neutral-800 flex flex-col md:flex-row items-center'>
 
 
-                    <div className='flex items-center'>
-                        
+                    <div className='flex flex-col md:flex-row items-center'>
+
                         {user && (
                             <>
                                 <img src={user.profileImage || ''} alt="Profile Image" className='w-20 h-20 rounded-full' />
                                 <h1 className='ml-4 text-3xl mr-2 font-bold'>{user.username}</h1>
                             </>
                         )}
-                        <div className='ml-4 mr-2 bg-yellow-600 px-4 rounded-full ml-auto text-sm'>
+                        <div className='ml-4 mr-2 bg-yellow-600 px-4 rounded-full text-sm mt-2 md:mt-0'>
                             <p>
                                 pro
                             </p>
                         </div>
-                        <div className='ml-4 bg-red-600 px-4 rounded-full ml-auto text-sm'>
+                        <div className='ml-4 bg-red-600 px-4 rounded-full text-sm mt-2 md:mt-0'>
                             <p>
                                 developer
                             </p>
@@ -90,111 +90,124 @@ export default function Create() {
 
 
 
-                    <div className='ml-auto'>
-                      
-                            {/*followers following*/}
-                            <div className=' px-2   ml-auto text-lg flex items-center gap-x-4'>
-                                <p>
-                                    {user && ( 
-                                        <>
-                                    <span className='font-bold'> Followers</span> {user.followers || '0'}
-                                    <span className='font-bold ml-4'> Following</span> {user.following || '0'}
-                                        </>
-                                    )}
+                    <div className='lg:ml-auto mt-4 md:mt-0 flex flex-col md:flex-row items-center'>
 
-                                </p>
+                        {/*followers following*/}
+                        <div className='px-2 text-lg flex flex-col md:flex-row items-center gap-x-4'>
+                            <p>
+                                {user && (
+                                    <>
+                                        <span className='font-bold'> Followers</span> {user.followers || '0'}
+                                        <span className='font-bold ml-4'> Following</span> {user.following || '0'}
+                                    </>
+                                )}
+                            </p>
 
-                                <div className='flex items-center gap-x-2'>
-                                <button className='bg-blue-600 px-4 rounded-full ml-auto text-sm'>
+                            <div className='flex items-center gap-x-2 mt-2 md:mt-0'>
+                                <button className='bg-blue-600 px-4 rounded-full text-sm'>
                                     <i class="fas fa-user-plus"></i>
                                 </button>
-                                <button className='bg-red-600 px-4 rounded-full ml-auto text-sm'>
+                                <button className='bg-red-600 px-4 rounded-full text-sm'>
                                     <i class="fas fa-flag"></i>
                                 </button>
-                                </div>
-
                             </div>
+
                         </div>
                     </div>
+                </div>
 
 
-                    <div className='mt-10 grid grid-cols-6 gap-x-4 gap-y-4 w-full'>
-  
-                    <div className='col-span-2 bg-neutral-800 px-4 py-4' style={{ height: '400px', overflowY: 'auto' }}>
+                <div className='mt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 lg:gap-x-4 gap-y-4 ' >
 
-                        
-<h1 className='text-2xl text-neutral-400 font-bold'>ACTIVITY FEED</h1>
+
+
+                    <div className='w-full '>
+
+                        <div className='col-span-2   bg-neutral-800 px-4 py-4' >
+                            <h1 className='text-2xl text-neutral-400 font-bold'>ACTIVITY FEED</h1>
                             {user && (
                                 <h1 className='mx-auto my-auto text-neutral-600'>Looks like {user.username} hasn't been that active.</h1>
                             )}
 
-</div>
-              
-             
-                        <div className='col-span-4'>
-
-                            <div className='grid grid-cols-1 gap-y-4 '>
-                                <div>
-                            <div className='pb-2'>
-                                <ul className='flex w-full list-none text-neutral-500'>
-                                    {['LIKED CHALLENGES', 'WRITEUPS', 'CREATED CHALLENGES', 'BADGES'].map(tab => (
-                                        <li
-                                            key={tab}
-                                            className={`mr-4 cursor-pointer ${activeTab === tab ? 'border-b-2 border-blue-600 text-blue-600 font-semibold' : ''}`}
-                                            onClick={() => setActiveTab(tab)}
-                                        >
-                                            {tab}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                           <div className='bg-neutral-800 px-4 py-4'>
-
-                        
-                           {renderContent()}
-                           </div>
-                           </div>
-                           <div className='bg-neutral-800 px-4 py-4'>
-
-                        {user && (
-                            <h1 className='text-2xl text-neutral-400 font-bold uppercase'>ABOUT {user.username}</h1>
-                            )}
-
-                            <p className='text-neutral-400'>
-                                {user && user.bio }
-                            </p>
-                        
-               </div>             
-</div>
                         </div>
-                        <div className='col-span-2 '>
 
-                                <div className='grid grid-cols-1 gap-y-4'>
+                        <div className='grid grid-cols-1 gap-y-4'>
                             <div className='bg-neutral-800 px-4 py-4'>
-                            <h1 className='text-2xl text-neutral-400 font-bold'>SKILL CHART</h1>
-                            {user && (
-                                <h1 className='mx-auto my-auto text-neutral-600'>It seems that {user.username} hasn't solved any challenges yet.</h1>
-                            )}
-                            </div>
+                                <h1 className='text-2xl text-neutral-400 font-bold'>SKILL CHART</h1>
+                                {user && (
+                                    <h1 className='mx-auto my-auto text-neutral-600'>It seems that {user.username} hasn't solved any challenges yet.</h1>
+                                )}
+                         
 
-                                     <div className='bg-neutral-800 px-4 py-4'>
-                            <h1 className='text-2xl text-neutral-400 font-bold'>NERD STATS</h1>
-                            {user && (
-                              <div className='text-white text-sm'>
-                                    <p>Account Created Date : {new Date(user.createdAt).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric', minute:'numeric', hour:'numeric'})}</p>
-                                    <p>Total Challenges: {user.totalChallenges || '0'}</p>
-                                    <p>Total Writeups: {user.totalWriteups || '0'}</p>
-                                    <p>Total Badges: {user.totalBadges || '0'}</p>
-                                </div>
-                            )}
+                            <div className='bg-neutral-800 gap-y-4 mt-6'>
+                                <h1 className='text-2xl text-neutral-400 font-bold'>NERD STATS</h1>
+                                {user && (
+                                    <div className='text-white text-xs'>
+                                        <p>Creation Date : {new Date(user.createdAt).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric', minute: 'numeric', hour: 'numeric' })}</p>
+                                        <p>Total Challenges: {user.totalChallenges || '0'}</p>
+                                        <p>Total Writeups: {user.totalWriteups || '0'}</p>
+                                        <p>Total Badges: {user.totalBadges || '0'}</p>
+                                    </div>
+                                )}
                             </div>
                             </div>
-
                         </div>
-
-
-                 
                     </div>
+                    <div className='col-span-3'>
+
+                        <div className='grid grid-cols-1 gap-y-4 '>
+                            <div>
+                                <div className='pb-2'>
+                                    <ul className='hidden md:flex w-full list-none text-neutral-500'>
+                                        {['LIKED CHALLENGES', 'WRITEUPS', 'CREATED CHALLENGES', 'BADGES'].map(tab => (
+                                            <li
+                                                key={tab}
+                                                className={`mr-4 cursor-pointer ${activeTab === tab ? 'border-b-2 border-blue-600 text-blue-600 font-semibold' : ''}`}
+                                                onClick={() => setActiveTab(tab)}
+                                            >
+                                                {tab}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                    <select
+                                        className='md:hidden w-full border-none px-4 w-1/2 bg-neutral-800 text-neutral-50  rounded'
+                                        value={activeTab}
+                                        onChange={(e) => setActiveTab(e.target.value)}
+                                    >
+                                        {['LIKED CHALLENGES', 'WRITEUPS', 'CREATED CHALLENGES', 'BADGES'].map(tab => (
+                                            <option key={tab} value={tab}>
+                                                {tab}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className='bg-neutral-800 px-4 py-4'>
+
+
+                                    {renderContent()}
+                                </div>
+                            </div>
+                            <div className='bg-neutral-800 px-4 py-4'>
+
+                                {user && (
+                                    <h1 className='text-2xl text-neutral-400 font-bold uppercase'>ABOUT {user.username}</h1>
+                                )}
+
+                                <p className='text-neutral-400'>
+                                    {user && user.bio}
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-span-2 '>
+
+
+                    </div>
+
+
+
+                </div>
 
             </main>
 
@@ -207,4 +220,3 @@ export default function Create() {
         </>
     );
 }
-
