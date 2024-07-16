d77a0d6afd2872fb745682e0d5b4fc1e0b771f77
Kshitij Kochhar <kshitij@ctfguide.com>
2024-07-16 15:08:38 -0400

    [feat] back button on following/follower feed implemented as well as pagination UI

--- src/pages/users/v2/[user].jsx
+++ src/pages/users/v2/[user].jsx
@@ -17,7 +17,10 @@ export default function Create() {
   const [user, setUser] = useState(null);
   const [activeTab, setActiveTab] = useState('LIKED CHALLENGES');
   const [isBioExpanded, setIsBioExpanded] = useState(false);
-  const [displayMode, setDisplayMode] = useState(null);
+  const [displayMode, setDisplayMode] = useState('default');
+  const [mutuals, setMutuals] = useState([]);
+  const [page, setPage] = useState(0); // Initial page
+  const [totalPages, setTotalPages] = useState(0); // Total pages
 
   const sampleUser = {
     username: 'testAccount',
@@ -97,6 +100,45 @@ export default function Create() {
         console.log(err);
       });
   }, [router.query.user]);
+
+  // Get The User's Following
+  useEffect(() => {
+    const storedUser = localStorage.getItem('username');
+    if (storedUser) {
+      setUser(storedUser);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      const fetchData = async () => {
+        try {
+          const endPoint =
+            process.env.NEXT_PUBLIC_API_URL +
+            '/followers/' +
+            user +
+            `/mutuals?page=${page}`;
+          const result = await request(endPoint, 'GET');
+          if (result && result.mutuals) {
+            setMutuals(result.mutuals);
+            setTotalPages(result.lastPage + 1);
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      };
+      fetchData();
+    }
+  }, [user, page]);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 0 && newPage < totalPages) {
+      setPage(newPage);
+    }
+  };
+
+  const nextPage = () => handlePageChange(page + 1);
+  const prevPage = () => handlePageChange(page - 1);
 
   return (
     <>
@@ -225,55 +267,20 @@ export default function Create() {
           <div className="col-span-3 border-t-4 border-blue-600">
             {displayMode === 'followers' && (
               <>
-              <div className="bg-neutral-800 px-4 py-4">
-                <div className="flex">
-                  <i class="fas fa-angle-double-left mr-2 py-1.5 text-2xl text-white hover:text-gray-400"></i>
-                  <h1 className="mb-4 text-2xl font-bold uppercase text-white">
-                    {user.username}'s Followers
-                  </h1>
-                </div>
-
-                {true ? (
-                  <div className="gap-4">
-                    <main className="flex justify-center p-4">
-                      <div className="grid max-w-7xl grid-cols-1 gap-5 sm:grid-cols-1 lg:grid-cols-2">
-                        {sampleData.mutuals.length > 0 ? (
-                          sampleData.mutuals.map((mutual, index) => (
-                            <FriendCard
-                              key={index}
-                              data={mutual}
-                              mutual={true}
-                            />
-                          ))
-                        ) : (
-                          <p className="col-span-3 text-lg text-white">
-                            You have no friends yet.
-                          </p>
-                        )}
-                      </div>
-                    </main>
-                  </div>
-                ) : (
-                  <p className="text-neutral-400">No followers yet.</p>
-                )}
-              </div>
-            </>
-            )}
-
-            {displayMode === 'following' && (
-              <>
                 <div className="bg-neutral-800 px-4 py-4">
                   <div className="flex">
-                    <i class="fas fa-angle-double-left mr-2 py-1.5 text-2xl text-white hover:text-gray-400"></i>
+                    <span onClick={() => setDisplayMode('default')}>
+                      <i class="fas fa-angle-double-left mr-2 py-1.5 text-2xl text-white hover:text-gray-400"></i>
+                    </span>
                     <h1 className="mb-4 text-2xl font-bold uppercase text-white">
-                      {user.username}'s Following
+                      {user.username}'s Followers
                     </h1>
                   </div>
 
                   {true ? (
                     <div className="gap-4">
-                      <main className="flex justify-center p-4">
-                        <div className="grid max-w-7xl grid-cols-1 gap-5 sm:grid-cols-1 lg:grid-cols-3">
+                      <div className="flex justify-center p-4">
+                        <div className="grid max-w-7xl grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
                           {sampleData.mutuals.length > 0 ? (
                             sampleData.mutuals.map((mutual, index) => (
                               <FriendCard
@@ -288,7 +295,67 @@ export default function Create() {
                             </p>
                           )}
                         </div>
-                      </main>
+                      </div>
+                    </div>
+                  ) : (
+                    <p className="text-neutral-400">No followers yet.</p>
+                  )}
+                  {totalPages > 0 && (
+                    <div className="flex items-center justify-center gap-x-5 py-4">
+                      <button
+                        onClick={prevPage}
+                        className="w-24 rounded-sm bg-gray-900 px-4 py-2 font-bold text-white hover:bg-gray-800 disabled:opacity-50 "
+                        disabled={page === 0}
+                      >
+                        Previous
+                      </button>
+                      <span className="text-white">
+                        Page {page + 1} of {totalPages}
+                      </span>
+                      <button
+                        onClick={nextPage}
+                        className="w-24 rounded-sm bg-gray-800 px-4 py-2 font-bold text-white hover:bg-gray-700 disabled:opacity-50"
+                        disabled={page + 1 >= totalPages}
+                      >
+                        Next
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </>
+            )}
+
+            {displayMode === 'following' && (
+              <>
+                <div className="bg-neutral-800 px-4 py-4">
+                  <div className="flex">
+                    <span onClick={() => setDisplayMode('default')}>
+                      <i class="fas fa-angle-double-left mr-2 py-1.5 text-2xl text-white hover:text-gray-400"></i>
+                    </span>
+                    <h1 className="mb-4 text-2xl font-bold uppercase text-white">
+                      {user.username}'s Following
+                    </h1>
+                  </div>
+
+                  {true ? (
+                    <div className="gap-4">
+                      <div className="flex justify-center p-4">
+                        <div className="grid max-w-7xl grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+                          {sampleData.mutuals.length > 0 ? (
+                            sampleData.mutuals.map((mutual, index) => (
+                              <FriendCard
+                                key={index}
+                                data={mutual}
+                                mutual={true}
+                              />
+                            ))
+                          ) : (
+                            <p className="col-span-3 text-lg text-white">
+                              You have no friends yet.
+                            </p>
+                          )}
+                        </div>
+                      </div>
                     </div>
                   ) : (
                     <p className="text-neutral-400">No followers yet.</p>
@@ -297,7 +364,7 @@ export default function Create() {
               </>
             )}
 
-            {!displayMode && (
+            {displayMode === 'default' && (
               <>
                 <div className="bg-neutral-800 px-4 py-4">
                   {user && (
