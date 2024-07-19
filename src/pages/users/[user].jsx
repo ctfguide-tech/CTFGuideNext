4bf0b6bb8a6d082657de74a44fe8afbbdcf72b5f
AbhiByreddy <54539671+ThunderBird260@users.noreply.github.com>
2024-07-19 00:36:56 -0400

    bug: fixed banner height to what it was in prod on users page

--- src/pages/users/[user].jsx
+++ src/pages/users/[user].jsx
@@ -123,7 +123,7 @@ export default function Create() {
                 <div>
                     <div
                         style={{ backgroundSize: "cover", backgroundImage: 'url("https://images.unsplash.com/photo-1633259584604-afdc243122ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80")' }}
-                        className="h-20 w-full object-cover lg:h-48"
+                        className="h-20 w-full object-cover lg:h-56"
                         alt=""
                     >
                     </div>
