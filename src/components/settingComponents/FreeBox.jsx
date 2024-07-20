f54929bd85c560753e27072707915bbf038d3d91
AbhiByreddy <54539671+ThunderBird260@users.noreply.github.com>
2024-07-20 01:24:43 -0400

    feat: updated list on billing

--- src/components/settingComponents/FreeBox.jsx
+++ src/components/settingComponents/FreeBox.jsx
@@ -23,7 +23,7 @@ export default function FreeBox() {
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
             <span className="ms-3 text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              2 team members
+              Acess to challenge catalog
             </span>
           </li>
           <li className="flex">
@@ -37,7 +37,7 @@ export default function FreeBox() {
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
             <span className="ms-3 text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              20GB Cloud storage
+              Limited terminal time
             </span>
           </li>
           <li className="flex">
@@ -51,7 +51,7 @@ export default function FreeBox() {
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
             <span className="ms-3 text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              Integration help
+              Limited ads
             </span>
           </li>
           <li className="flex line-through decoration-gray-500">
@@ -65,7 +65,7 @@ export default function FreeBox() {
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
             <span className="ms-3 text-base font-normal leading-tight text-gray-500">
-              Sketch Files
+              Custom banner images
             </span>
           </li>
           <li className="flex line-through decoration-gray-500">
@@ -79,7 +79,7 @@ export default function FreeBox() {
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
             <span className="ms-3 text-base font-normal leading-tight text-gray-500">
-              API Access
+              PRO badge
             </span>
           </li>
           <li className="flex line-through decoration-gray-500">
@@ -93,7 +93,7 @@ export default function FreeBox() {
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
             <span className="ms-3 text-base font-normal leading-tight text-gray-500">
-              Complete documentation
+              Colored Username
             </span>
           </li>
           <li className="flex line-through decoration-gray-500">
@@ -107,7 +107,7 @@ export default function FreeBox() {
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
             <span className="ms-3 text-base font-normal leading-tight text-gray-500">
-              24×7 phone & email support
+              Acess to upcoming beta features
             </span>
           </li>
         </ul>
