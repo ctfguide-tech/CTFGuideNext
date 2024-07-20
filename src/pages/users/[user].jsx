610284e3ba53dde41007c1488142e12a78136384
Pranav Ramesh <laphatize@protonmail.com>
2024-07-20 17:56:19 -0400

    feat: banner images

--- src/pages/users/[user].jsx
+++ src/pages/users/[user].jsx
@@ -502,8 +502,8 @@ export default function Create() {
                     <div
                         style={{
                             backgroundSize: 'cover',
-                            backgroundImage:
-                                'url("https://images.unsplash.com/photo-1633259584604-afdc243122ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80")',
+                            backgroundPosition: 'center', // Center the background image
+                            backgroundImage: `url(${user && user.bannerImage ? user.bannerImage : "https://images.unsplash.com/photo-1633259584604-afdc243122ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"})`,
                         }}
                         className="h-20 w-full object-cover lg:h-56"
                         alt=""
