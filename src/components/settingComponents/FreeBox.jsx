13957315acb96f59d7ce01a77f2b7a5af0dde991
AbhiByreddy <54539671+ThunderBird260@users.noreply.github.com>
2024-07-28 00:22:13 -0400

    feat: added new feature to billing cards

--- src/components/settingComponents/FreeBox.jsx
+++ src/components/settingComponents/FreeBox.jsx
@@ -83,7 +83,7 @@ export default function FreeBox() {
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
             <span className="ms-3 text-base font-normal leading-tight text-gray-500">
-              PRO badge
+              Larger file uploads
             </span>
           </li>
           <li className="flex line-through decoration-gray-500">
@@ -97,7 +97,7 @@ export default function FreeBox() {
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
             </svg>
             <span className="ms-3 text-base font-normal leading-tight text-gray-500">
-              Colored Username
+              PRO badge & Colored Username
             </span>
           </li>
           <li className="flex line-through decoration-gray-500">
