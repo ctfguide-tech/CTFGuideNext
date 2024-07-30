ee614040a6c56c035815e53d1e1617bc0b3433d1
Gardiunn <haukben8@gmail.com>
2024-07-29 21:26:43 -0400

    badge changes

--- src/components/profile/Badge.jsx
+++ src/components/profile/Badge.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 import { Tooltip } from 'react-tooltip';
 import CusTooltip from './BadgeOnhover';
 
-const Badge =  ({ active, createdAt, badgeName, badgeImage, badgeDescription, badgeReason }) => {
-    
-    
-    return (
-    <CusTooltip name={badgeName} description={badgeDescription} icon={<img src={badgeImage}/>} reason={badgeReason} date={createdAt}>
-    <div
-    className="border border-neutral-700 border-2 bg-neutral-800 align-center mx-auto w-full rounded-lg px-4 py-4 text-center duration-4000 min-h-[190px] min-w-[200px] transition ease-in-out hover:bg-neutral-700/40"
-    data-tooltip-id="badge-tooltip"
-    data-tooltip-place="top"
+const Badge = ({
+  active,
+  createdAt,
+  badgeName,
+  badgeImage,
+  badgeDescription,
+  badgeReason,
+}) => {
+  return (
+    <CusTooltip
+      name={badgeName}
+      description={badgeDescription}
+      icon={<img src={badgeImage} />}
+      reason={badgeReason}
+      date={createdAt}
     >
-    <Tooltip id="badge-tooltip" />
-    <img
-      src={badgeImage}
-      width="100"
-      className="mx-auto mt-2 px-1"
-    />
-
-  </div>
-  </CusTooltip>
-)};
+      <div
+        className="bg-neutral-800 py-4 text-center transition ease-in-out hover:bg-neutral-700/40"
+        data-tooltip-id="badge-tooltip"
+        data-tooltip-place="top"
+      >
+        <Tooltip id="badge-tooltip" />
+        <img src={badgeImage} width="100" className="mx-auto" />
+      </div>
+    </CusTooltip>
+  );
+};
 
 export default Badge;
--- src/components/profile/v2/Badges.jsx
+++ src/components/profile/v2/Badges.jsx
@@ -7,95 +7,101 @@ import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import CusTooltip from '../BadgeOnhover';
 const Badges = ({ user }) => {
-
   const router = useRouter();
   const [badges, setUserBadges] = useState([]);
   const [loading, setLoading] = useState(true);
-  
+  const [currentPage, setCurrentPage] = useState(1);
+  const badgesPerPage = 25;
+
   useEffect(() => {
     const fetchUserBadges = async () => {
-      const response = await request(`${process.env.NEXT_PUBLIC_API_URL}/account/userBadges`, 'GET', null );
+      const response = await request(
+        `${process.env.NEXT_PUBLIC_API_URL}/account/userBadges`,
+        'GET',
+        null
+      );
       setUserBadges(response.badges);
       setLoading(false);
-    }
+    };
     setLoading(true);
     fetchUserBadges();
   }, [user]);
-  if (loading) return <div className='text-neutral-400'><Skeleton width="100%" baseColor="#363535" highlightColor="#615f5f"  /></div>;
-  console.log("USER BADGES:", badges);
-  
 
-  /*
-  Badge object returned:
-  badge: 
-    active: true
-    badgeBonus: 0
-    badgeDescription: "Extinguish those flames!"
-    badgeImage: ""
-    badgeName: "First HARD Challenge"
-    badgeReason: "Submit your first hard challenge"
-    id: 9
-  */
-    /*
-      "success": true,
-      "badges": [
-          {
-              "id": 1,
-              "createdAt": "2024-07-28T21:11:30.671Z",
-              "userId": "4eb33ad3-e477-4088-a726-d76dae33b01c",
-              "badgeId": 9,
-              "badge": {
-                  "id": 9,
-                  "badgeName": "First HARD Challenge",
-                  "badgeDescription": "Extinguish those flames!",
-                  "badgeReason": "Submit your first hard challenge",
-                  "badgeImage": "",
-                  "badgeBonus": 0,
-                  "active": true
-              }
-          },
-          {
-              "id": 2,
-              "createdAt": "2024-07-28T21:11:30.704Z",
-              "userId": "4eb33ad3-e477-4088-a726-d76dae33b01c",
-              "badgeId": 2,
-              "badge": {
-                  "id": 2,
-                  "badgeName": "First Challenge",
-                  "badgeDescription": "A first of many",
-                  "badgeReason": "Complete your first challenge",
-                  "badgeImage": "",
-                  "badgeBonus": 0,
-                  "active": true
-              }
-          },
-      */
+  if (loading)
+    return (
+      <div className="text-neutral-400">
+        <Skeleton width="100%" baseColor="#363535" highlightColor="#615f5f" />
+      </div>
+    );
+
+  console.log('USER BADGES:', badges);
+
+  const totalPages = Math.ceil(badges.length / badgesPerPage);
+  const startIndex = (currentPage - 1) * badgesPerPage;
+  const endIndex = startIndex + badgesPerPage;
+  const currentBadges = badges.slice(startIndex, endIndex);
+
+  const rowsNeeded = Math.ceil(currentBadges.length / 5);
 
   return (
-  <div className="grid grid-cols-5 gap-x-2 gap-y-2 rounded-sm bg-neutral-800 mt-4">
-          {badges && badges.length > 0 ? (
-            
-            badges.map((badge) => (
-             
-              <Badge
-                key={badge.id}
-                active={badge.badge.active}
-                badgeName={badge.badge.badgeName}
-                badgeImage={"../badges/group2.png"}
-                badgeDescription={badge.badge.badgeDescription}
-                badgeReason={badge.badge.badgeReason}
-                createdAt={badge.createdAt}
-              />
-            ))
-          ) : (
-            <div className="align-center duration-4000 col-span-5 mx-auto min-h-[190px] w-full min-w-[200px] rounded-sm border border-2 border-neutral-700 bg-neutral-800 px-4 py-4 text-center transition ease-in-out hover:bg-neutral-700/40" data-tooltip-content="Complete challenges to earn badges!" data-tooltip-id="badge-tooltip" data-tooltip-place="top">
-              {user && <Tooltip id="badge-tooltip" />}
-              <img src={'/CuteKana.png'} width="100" className="mx-auto mt-2 px-1" />
-              <h1 className="mx-auto mt-2 text-center text-xl text-white">No Badges Yet...</h1>
-            </div>
-          )}
-    
-    </div>
+    <>
+      <div
+        className="grid grid-cols-5 grid-rows-5 bg-neutral-800"
+        style={{ gridTemplateRows: `repeat(${rowsNeeded}, minmax(0, 1fr))` }}
+      >
+        {currentBadges && currentBadges.length > 0 ? (
+          currentBadges.map((badge) => (
+            <Badge
+              key={badge.id}
+              active={badge.badge.active}
+              badgeName={badge.badge.badgeName}
+              badgeImage={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/badges/group2.png`}
+              badgeDescription={badge.badge.badgeDescription}
+              badgeReason={badge.badge.badgeReason}
+              createdAt={badge.createdAt}
+            />
+          ))
+        ) : (
+          <div
+            className="col-span-5 mx-auto min-h-[190px] w-full min-w-[200px] rounded-sm border-2 border-neutral-700 bg-neutral-800 px-4 py-4 text-center transition ease-in-out hover:bg-neutral-700/40"
+            data-tooltip-content="Complete challenges to earn badges!"
+            data-tooltip-id="badge-tooltip"
+            data-tooltip-place="top"
+          >
+            {user && <Tooltip id="badge-tooltip" />}
+            <img
+              src={'/CuteKana.png'}
+              width="100"
+              className="mx-auto mt-2 px-1"
+            />
+            <h1 className="mx-auto mt-2 text-center text-xl text-white">
+              No Badges Yet...
+            </h1>
+          </div>
+        )}
+      </div>
+      <div className="mx-auto mt-8 text-center">
+        <button
+          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+          disabled={currentPage === 1}
+          className="rounded-lg bg-neutral-600 p-2 text-white"
+        >
+          Previous
+        </button>
+        <span className="m-4 text-white">
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          onClick={() =>
+            setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+          }
+          disabled={currentPage === totalPages}
+          className="rounded-lg bg-neutral-600 px-6 py-2 text-white"
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 };
 
