3a7546b0d370234134a2b677e1c7019615c32633
AbhiByreddy <54539671+ThunderBird260@users.noreply.github.com>
2024-08-04 01:32:53 -0400

    feat: added mapping for patchnotes

--- src/components/PatchNote.jsx
+++ src/components/PatchNote.jsx
@@ -1,20 +1,26 @@
 import { MarkdownViewer } from '@/components/MarkdownViewer';
 import React from 'react';
 
-export function PatchNote({props}) {
+export function PatchNote({description, date, title, version}) {
   return (
     <>
       <div className="mb-3 mt-3 rounded-lg bg-neutral-800 px-4">
         <div className="mb-4 pt-2">
-          <h1 className="text-xl font-semibold text-blue-600 ">
-            Version 2.0.0
-            <span className="pl-3 text-right text-lg text-white">8/2/2024</span>
+          <div className='flex justify-center items-center'>
+            <div>
+            <h1 className="text-xl font-semibold text-blue-600 ">
+            {version}
+            <h2 className="text-lg text-white ">{title}</h2>
           </h1>
-          <h2 className="text-lg text-white ">Title</h2>
+            </div>
+            <div className='ml-auto'>
+            <h2 className="pl-3 text-right text-lg text-white">{date}</h2>
+            </div>
+          </div>
         </div>
         <div className="px-5 py-5 ">
           <div contentEditable={false}>
-            <MarkdownViewer className="text-white font-medium" content="Welcome to CTFGuide 2.0!"/>
+            <MarkdownViewer className="text-white font-medium" content={description}/>
           </div>
         </div>
       </div>
--- src/pages/patch-notes.jsx
+++ src/pages/patch-notes.jsx
@@ -3,8 +3,30 @@ import { Footer } from '@/components/Footer';
 import { StandardNav } from '@/components/StandardNav';
 import { useEffect, useState } from 'react';
 import {PatchNote} from '@/components/PatchNote';
+import request from '@/utils/request';
 
 export default function PatchNotes() {
+    const[patchNotes, setPatchNotes] = useState([{
+        description: "Description of the patch note",
+        date: "8/4/2024",
+        title: "CTFGuide Release",
+        version: "Version 1.0.0"
+    }]);
+    
+    async function getPatchNotes() {
+        //fetch patch notes
+        const response = await request(`${process.env.NEXT_PUBLIC_API_URL}/patchNotes`, 'GET', null);
+        if(!response.error) {
+        setPatchNotes(response);
+        }
+    }
+
+    useEffect(() => {
+        getPatchNotes();
+    }, []);
+
+    console.log('patchNotes:', patchNotes);
+
 return (
     <>
         <Head>
@@ -27,6 +49,13 @@ return (
             <h2 className='text-xl text-white text-left align-middle font-bold'>
                 A changelog of updates to the <span className='text-blue-600'>CTFGuide</span> platform
             </h2>
+            {patchNotes &&
+                patchNotes.map((patchNote) => {
+                    return (
+                        <PatchNote description={patchNote.description} date={patchNote.date} title={patchNote.title} version={patchNote.version}/>
+                    );
+                })
+            }
            <PatchNote description ="we did a whole lot!" date="7/31/24" title="CTFGuide Release" version="Version 1.0.0"/>
         </div>
         <div className='flex w-full h-full grow basis-0'></div>
