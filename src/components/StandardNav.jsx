ac8987d504d8bb5800c893f562937a1135dbe7c4
Pranav Ramesh <laphatize@protonmail.com>
2024-09-20 00:31:55 -0400

    Update StandardNav.jsx

--- src/components/StandardNav.jsx
+++ src/components/StandardNav.jsx
@@ -677,7 +677,7 @@ export function StandardNav({ guestAllowed, alignCenter = true }) {
       />
       <Upgrade open={upgradeModalOpen} setOpen={setUpgradeModalOpen} />
 
-        <div className="mx-auto  w-full bg-yellow-800 py-1 text-center  text-sm text-white ">
+        <div className="mx-auto  hidden w-full bg-yellow-800 py-1 text-center  text-sm text-white ">
           <h1 className="mx-auto  px-4 text-left">
               Our server hosting provider is experiencing issues which may result in CTFGuide not working as expected. Join our <a className="font-bold" href="https://discord.gg/BwbCYHVX">Discord</a> for updates.
           </h1>
