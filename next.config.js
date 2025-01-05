f5974f7d970faf4bdc496682126cdc463d7039fb
Pranav Ramesh <laphatize@protonmail.com>
2025-01-05 00:15:53 -0500

    feat: cors fix

--- next.config.js
+++ next.config.js
@@ -15,47 +15,8 @@ const nextConfig = {
     esmExternals: 'loose',
   },
   transpilePackages: ['@uiw/react-md-editor', '@uiw/react-markdown-preview'],
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: '*',
-          },
-        ],
-      },
-    ];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-    ],
-  },
+
+
 };
 
 module.exports = withPWA(removeImports(nextConfig));
