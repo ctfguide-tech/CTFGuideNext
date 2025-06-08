935a53ddf5609f6c5eafe66083e946f7eb6cab19
AbhiByreddy <54539671+ThunderBird260@users.noreply.github.com>
2025-06-08 12:24:10 -0400

    Updated sidebar.jsx

--- src/components/settingComponents/sidebar.jsx
+++ src/components/settingComponents/sidebar.jsx
@@ -8,31 +8,35 @@ export default function Sidebar() {
       className="mt-10 flex-none border-r pl-10 pr-10 text-gray-900 sm:pl-4 sm:pr-4"
       style={{ borderColor: '#212121' }}
     >
-      <ul className="mr-2 py-2">
+      <ul className="mr-2 list-none py-2">
         <li className="py-1">
           <Link href="../settings" passHref>
-            <div className="px-2 py-1 text-lg font-medium text-white hover:text-gray-400 sm:text-base">
+            <div className="flex items-center px-2 py-1 text-lg font-medium text-white hover:text-gray-400 sm:text-base">
+              <div className="mr-2 h-1.5 w-1.5 rounded-full bg-blue-600"></div>
               General
             </div>
           </Link>
         </li>
         <li className="py-1">
           <Link href="../settings/security" passHref>
-            <div className="px-2 py-1 text-lg font-medium text-white hover:text-gray-400 sm:text-base">
+            <div className="flex items-center px-2 py-1 text-lg font-medium text-white hover:text-gray-400 sm:text-base">
+              <div className="mr-2 h-1.5 w-1.5 rounded-full bg-blue-600"></div>
               Security
             </div>
           </Link>
         </li>
         <li className="py-1">
           <Link href="../settings/preferences" passHref>
-            <div className="px-2 py-1 text-lg font-medium text-white hover:text-gray-400 sm:text-base">
+            <div className="flex items-center px-2 py-1 text-lg font-medium text-white hover:text-gray-400 sm:text-base">
+              <div className="mr-2 h-1.5 w-1.5 rounded-full bg-blue-600"></div>
               Email Preferences
             </div>
           </Link>
         </li>
         <li className="py-1">
           <Link href="../settings/billing" passHref>
-            <div className="px-2 py-1 text-lg font-medium text-white hover:text-gray-400 sm:text-base">
+            <div className="flex items-center px-2 py-1 text-lg font-medium text-white hover:text-gray-400 sm:text-base">
+              <div className="mr-2 h-1.5 w-1.5 rounded-full bg-blue-600"></div>
               Billing
             </div>
           </Link>
